8c7597e62e2459c3ef149189dd8c466c5498828d
Carlos Bruno Bonfim <33705093+CarlosBonfim@users.noreply.github.com>
2024-06-13 14:16:51 -0400

    Criada auntenticação via breeze

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RiscoController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -9,18 +10,31 @@ use App\Http\Controllers\RiscoController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
-Route::get('/welcome', function () {
+Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/', function (){
-	return redirect()->route('riscos.home');
+
+
+
+
+
+Route::middleware('auth')->group(function () {
+		Route::get('/', function (){return redirect()->route('riscos.home');});
+
+
+		Route::get('/home', [RiscoController::class, 'home'])->name('riscos.home');
+
+		Route::get('/risco/novo', [RiscoController::class,'create'])->name('riscos.create');
+		Route::post('/risco/criar', [RiscoController::class, 'store'])->name('riscos.store');
+		
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/home', [RiscoController::class, 'home'])->name('riscos.home');
-Route::get('/risco/novo', [RiscoController::class,'create'])->name('riscos.create');
-Route::post('/risco/criar', [RiscoController::class, 'store'])->name('riscos.store');
+require __DIR__.'/auth.php';
