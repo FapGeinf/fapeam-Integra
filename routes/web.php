f3644aae6a546f45684f280ad2bd8fec8f80afe4
Carlos Bruno Bonfim <33705093+CarlosBonfim@users.noreply.github.com>
2024-06-13 14:52:50 -0400

    Revert "Criada auntenticação via breeze"
    This reverts commit 8c7597e62e2459c3ef149189dd8c466c5498828d.

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
-use App\Http\Controllers\RiscoController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RiscoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -10,31 +9,18 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
-
-
-
-
-
-Route::middleware('auth')->group(function () {
-		Route::get('/', function (){return redirect()->route('riscos.home');});
-
-
-		Route::get('/home', [RiscoController::class, 'home'])->name('riscos.home');
-
-		Route::get('/risco/novo', [RiscoController::class,'create'])->name('riscos.create');
-		Route::post('/risco/criar', [RiscoController::class, 'store'])->name('riscos.store');
-		
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::get('/', function (){
+	return redirect()->route('riscos.home');
 });
 
-require __DIR__.'/auth.php';
+Route::get('/home', [RiscoController::class, 'home'])->name('riscos.home');
+Route::get('/risco/novo', [RiscoController::class,'create'])->name('riscos.create');
+Route::post('/risco/criar', [RiscoController::class, 'store'])->name('riscos.store');
