89e4b2693975decf8d4cc83b00093967212f66fa
LuizFelipe2023 <luizfelipefroisneves@gmail.com>
2024-06-14 12:19:27 -0400

    Store precisa ser revisado

--- app/Models/Risco.php
+++ app/Models/Risco.php
@@ -1,34 +1,43 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Unidade;
+use App\Models\User;
+use App\Models\Monitoramento;
+use App\Models\Resposta;
 
 class Risco extends Model
 {
     use HasFactory;
-		protected $fillable = [
-			'riscoEvento',
-			'riscoCausa',
-			'riscoConsequencia',
-			'riscoAvaliacao',
-			'unidadeRiscoFK'
-		];
-	
-	public function unidade()
-	{
-		   return $this->belongsTo(Unidade::class, 'unidadeRiscoFK');
-	}
 
-	public function monitoramentos()
-	{
-		   return $this->hasMany(Monitoramento::class);
-	}
+    protected $fillable = [
+        'riscoEvento',
+        'riscoCausa',
+        'riscoConsequencia',
+        'riscoAvaliacao',
+        'userIdRisco',
+        'unidadeId' // Adicionando unidadeId aos campos preenchíveis
+    ];
 
-	public function respostas()
-	{
-		   return $this->hasMany(Resposta::class);
-	}
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'userIdRisco');
+    }
+
+    public function monitoramentos()
+    {
+        return $this->hasMany(Monitoramento::class);
+    }
+
+    public function respostas()
+    {
+        return $this->hasMany(Resposta::class);
+    }
+
+    public function unidade()
+    {
+        return $this->belongsTo(Unidade::class, 'unidadeId');
+    }
 }
--- resources/views/riscos/store.blade.php
+++ resources/views/riscos/store.blade.php
@@ -1,116 +1,118 @@
 @extends('layouts.app')
+
 @section('content')
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário de Risco</title>
+    <style>
+        .form_risco {
+            border: solid 1px greenyellow;
+            width: 100%;
+            position: absolute;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
 
-		.form_risco{
-			border: solid 1px greenyellow;
-			width: 100%;
-			position: absolute;
-			display: flex;
-			align-items: center;
-			justify-content: center;
-		}
-		.form_create{
-			border: solid 1px red;
-			display: flex;
-			align-items: center;
-			flex-direction: column;
-			margin: 10px;
-			padding: 10px;
-			width: 20%;
-		}
-		.textInput{
-			width: 100%;
-			height: 10%;
-		}
-		.selection{
-			margin: 10px;
-		}
-		@media screen and (max-width: 900px) {
-			.form_risco{
-				width: 100%;
-			}
-			.form_create{
-				width: 75%;
-			}
-			.textInput{
-				width: 100%;
-			}
-		}
+        .form_create {
+            border: solid 1px red;
+            display: flex;
+            align-items: center;
+            flex-direction: column;
+            margin: 10px;
+            padding: 10px;
+            width: 20%;
+        }
 
-	</style>
-		<script>
-		let cont = 0;
+        .textInput {
+            width: 100%;
+            height: 10%;
+        }
 
-		function addMonitoramentos(){
-			let controleSugerido = document.createElement('input');
-			controleSugerido.setAttribute('type','text');
-			controleSugerido.setAttribute(`name`,`monitoramentos[${cont}][monitoramentoControleSugerido]`);
-			controleSugerido.setAttribute('placeholder','Monitoramento');
-			controleSugerido.setAttribute('class','textInput');
+        .selection {
+            margin: 10px;
+        }
 
-			let statusMonitoramento = document.createElement('input');
-			statusMonitoramento.type = `text`;
-			statusMonitoramento.name = `monitoramentos[${cont}][statusMonitoramento]`;
-			statusMonitoramento.placeholder = "Status do Monitoramento";
-			statusMonitoramento.classList = "textInput";
+        @media screen and (max-width: 900px) {
+            .form_risco {
+                width: 100%;
+            }
 
-      let execucaoMonitoramento = document.createElement('input');
-      execucaoMonitoramento.setAttribute('type','text');
-      execucaoMonitoramento.setAttribute('name',`monitoramentos[${cont}][execucaoMonitoramento]`);
-      execucaoMonitoramento.setAttribute('placeholder','Execução do Monitoramento');
-      execucaoMonitoramento.setAttribute('class','textInput');
+            .form_create {
+                width: 75%;
+            }
 
-			const br = document.createElement('br');
-
-
-			let monitoramentosDiv = document.getElementById('formCreate');
-			monitoramentosDiv.appendChild(controleSugerido);
-			monitoramentosDiv.appendChild(statusMonitoramento);
-      monitoramentosDiv.appendChild(execucaoMonitoramento);
-			cont++;
-		}
-
-	</script>
-	<title>Document</title>
+            .textInput {
+                width: 100%;
+            }
+        }
+    </style>
 </head>
 <body>
-	<div class="form_risco">
-		<form action="{{route('riscos.store')}}" method="post" class="form_create" id="formCreate">
-			@csrf
-			<label for="name">Evento</label>
-			<textarea type="text" name="riscoEvento" class="textInput" required></textarea>
+    <div class="form_risco">
+        <form action="{{ route('riscos.store') }}" method="post" class="form_create" id="formCreate">
+            @csrf
+            <label for="riscoEvento">Evento</label>
+            <textarea type="text" name="riscoEvento" class="textInput" required></textarea>
 
-			<label for="name">Causa</label>
-			<textarea type="text" name="riscoCausa" class="textInput" required></textarea>
+            <label for="riscoCausa">Causa</label>
+            <textarea type="text" name="riscoCausa" class="textInput" required></textarea>
 
-			<label for="name">Consequência</label>
-			<textarea type="text" name="riscoConsequencia" class="textInput" required></textarea>
+            <label for="riscoConsequencia">Consequência</label>
+            <textarea type="text" name="riscoConsequencia" class="textInput" required></textarea>
 
-			<label for="name">Avaliação</label>
-			<textarea type="text" name="riscoAvaliacao" class="textInput" required></textarea>
+            <label for="riscoAvaliacao">Avaliação</label>
+            <input type="number" name="riscoAvaliacao" class="textInput" required>
 
-			<label for="name">Unidade</label>
-			<select name="unidadeRiscoFK" class="selection" requireds>
-				<option selected disabled>Selecione uma unidade</option>
-				@foreach($unidades as $unidade )
-					<option value="{{$unidade->id}}">{{$unidade->unidadeNome}}</option>
-				@endforeach
-			</select>
-			<div id="monitoramentosDiv" class="monitoramento"></div>
-			<input type="button" onclick="addMonitoramentos()" value="Adicionar Monitoramento"></input>
+            <label for="unidadeId">Unidade</label>
+            <select name="unidadeId" class="selection" required>
+                <option selected disabled>Selecione uma unidade</option>
+                @foreach($unidades as $unidade)
+                    <option value="{{ $unidade->id }}">{{ $unidade->unidadeNome }}</option>
+                @endforeach
+            </select>
 
-			<input type="submit" value="Salvar">
+            <div id="monitoramentosDiv" class="monitoramento"></div>
+            <input type="button" onclick="addMonitoramentos()" value="Adicionar Monitoramento">
+            <button type="submit">Salvar</button>
+        </form>
+    </div>
 
-		</form>
-	</div>
+    <script>
+        let cont = 0;
 
+        function addMonitoramentos() {
+            let controleSugerido = document.createElement('input');
+            controleSugerido.type = 'text';
+            controleSugerido.name = `monitoramentos[${cont}][monitoramentoControleSugerido]`;
+            controleSugerido.placeholder = 'Monitoramento';
+            controleSugerido.classList = 'textInput';
+            controleSugerido.value = ''; // Defina o valor padrão aqui se necessário
+
+            let statusMonitoramento = document.createElement('input');
+            statusMonitoramento.type = 'text';
+            statusMonitoramento.name = `monitoramentos[${cont}][statusMonitoramento]`;
+            statusMonitoramento.placeholder = 'Status do Monitoramento';
+            statusMonitoramento.classList = 'textInput';
+            statusMonitoramento.value = ''; // Defina o valor padrão aqui se necessário
+
+            let execucaoMonitoramento = document.createElement('input');
+            execucaoMonitoramento.type = 'text';
+            execucaoMonitoramento.name = `monitoramentos[${cont}][execucaoMonitoramento]`;
+            execucaoMonitoramento.placeholder = 'Execução do Monitoramento';
+            execucaoMonitoramento.classList = 'textInput';
+            execucaoMonitoramento.value = ''; // Defina o valor padrão aqui se necessário
+
+            let monitoramentosDiv = document.getElementById('monitoramentosDiv');
+            monitoramentosDiv.appendChild(controleSugerido);
+            monitoramentosDiv.appendChild(statusMonitoramento);
+            monitoramentosDiv.appendChild(execucaoMonitoramento);
+            cont++;
+        }
+    </script>
 </body>
 </html>
-
 @endsection
