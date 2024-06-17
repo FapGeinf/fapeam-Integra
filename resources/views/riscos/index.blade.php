9ec2c2073abe9c837154146bdbd1e16543ab6da4
Leo S. <contato.prof.leonardo@gmail.com>
2024-06-17 16:32:16 -0400

    Atualização Página Inicial

--- resources/views/riscos/index.blade.php
+++ resources/views/riscos/index.blade.php
@@ -1,121 +1,134 @@
 @extends('layouts.app')
-
 @section('content')
-
 @section('title') {{'Página Inicial'}} @endsection
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="{{ asset('css/home.css')}}">
-  <script src="https://code.jquery.com/jquery-3.5.1.js" defer></script>
-  <script src="https://cdn.datatables.net/1.11.0/js/jquery.dataTables.min.js" defer></script>
-  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.14/css/jquery.dataTables.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	<title>Document</title>
+	<link rel="stylesheet" href="{{asset('css/home.css')}}">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.2.3/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.js"></script>
 </head>
 <body>
 
-  <div class="container-fluid p-30 pt-5">
-    <div class="row">
-      <div class="col-md-12 main-datatable">
-        <div class="card_body">
-          <div class="buttonNewRisk">
-            <span class="glyphicon glyphicon-plus"></span>
-            <a href="{{route('riscos.create')}}">
-              Novo Relatório</a>
-            </a>
-          </div>
+	<div class="container-fluid p-30">
+		<div class="col-12 main-datatable">
 
-          <div class="container-fluid">
-            <div class="overflow-x">
-              <table style="width: 100%" id="relatoriosTable" class="table cust-datatable no-footer">
-                <thead class="titles">
-                  <tr>
+			<div class="divButtonNewRisk">
+				@if(session('error'))
+				<script>alert('{{ session('error') }}');</script>
+				@endif
 
-                    <th style="min-width:65px;">Ano</th>
-                    <th style="min-width:150px;">Unidade<br><span class="subtitles">(Especificidade)</span></th>
-                    <th style="min-width:150px;">Responsável</th>
-                    <th style="min-width:65px;">Evento de Risco Inerente<br><span class="subtitles">Causas/Consequências</span></th>
-                    <th style="min-width:65px;">Classificação de Risco <br><span class="subtitles">RI(PxI)</span></th>
-                    <th style="min-width:70px;">Controle Sugerido<br><span class="subtitles">Execução/Status</span></th>   
-                    <th style="min-width:95px;">Avaliação<br><span class="subtitles">Recomendação</span></th>   
-                       
-                  </tr>
-                </thead>
+				<div id="newRiskButtonDiv" class="buttonNewRisk">
+					@if (Auth::user()->unidade->unidadeTipoFK == 1)
+						<a href="{{route('riscos.create')}}" class=""><i class="bi bi-plus-lg"></i> Novo Risco</a>
+					@endif
+				</div>
+			</div>
+			
+			<div class="container-fluid">
+				<table id="tableHome" class="table cust-datatable">
+					<thead class="">
+						<tr>
+							<th>Unidade</th>
+							<th>Evento de Risco</th>
+							<th>Causa</th>
+							<th>Consequência</th>
+							<th>Avaliação</th>
+						</tr>
+					</thead>
+					<tbody>
+						@foreach ($riscos as $risco)
+							<tr style="cursor: pointer;" onclick="window.location='{{ route('riscos.show', $risco->id) }}';">
+								<td>{{$risco->unidade->unidadeNome}}</td>
+								<td>{!!$risco->riscoEvento!!}</td>
+								<td>{!!$risco->riscoCausa!!}</td>
+								<td>{!!$risco->riscoConsequencia!!}</td>
+								<td>{{$risco->riscoAvaliacao}}</td>
+							</tr>
+						@endforeach
+					</tbody>
+				</table>
+			</div>
+			</div>
+		</div>
 
-                <tbody>
+		<footer class="rodape">
+			<div class="riskLevelDiv">
+				<span>Nível de Risco:</span>
+				<span class="riskLevel1">Baixo</span>
+				<span class="riskLevel2">Médio</span>
+				<span class="riskLevel3">Alto</span>
+			</div>
+		</footer>
+				
 
-                @foreach ($riscos as $risco)
-                <tr>
-                  <td>{{$risco->riscoEvento}}</td>
-                  <td>{{$risco->riscoCausa}}</td>
-                  <td>{{$risco->riscoConsequencia}}</td>
-                  <td>{{$risco->riscoAvaliacao}}</td>
-                  <td>{{$risco->unidade->unidadeNome}}</td>
-                </tr>
-                @endforeach
 
-                {{-- <tr>
-                  <td>2024</td>
-                  <td>Unidade de Controle Interno - UCI</td>
-                  <td>Luis Felipe dos Santos</td>
-                  <td>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</td>
-                  <td style="color: red; font-weight:bold;">Alta</td>
-                  <td>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</td>
-                  <td>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</td>
-                </tr>
+		<script>
+$(document).ready(function(){
+    var table = $('#tableHome').DataTable({
+        language: {
+					url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/pt-BR.json',
+            search: "Procurar:",
+            lengthMenu: "Relatórios: _MENU_",
+            info: 'Mostrando página _PAGE_ de _PAGES_',
+            infoEmpty: 'Sem relatórios disponíveis para visualização',
+            infoFiltered: '(Filtrados do total de _MAX_ relatórios)',
+            zeroRecords: 'Nada encontrado. Se achar que isso foi um erro, contate o suporte.',
+            paginate: {
+						next: "Próximo",
+						previous: "Anterior"
+						}
+						
+        },
+        initComplete: function(){
+            // Cria um contêiner para alinhar os elementos na mesma linha
+            var divContainer = $('<div class="divContainer" style="display: flex; justify-content: space-between;"></div>');
 
-                <tr>
-                  <td>2024</td>
-                  <td>Gerência de Informática - GEINF</td>
-                  <td>Felipe Andrey</td>
-                  <td>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</td>
-                  <td style="color: green; font-weight:bold;">Baixo</td>
-                  <td>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</td>
-                  <td>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</td>
-                </tr>
+            // Move o botão de novo risco para a esquerda
+            var divButtonNewRisk = $('<div class="divButtonNewRisk"></div>');
+            divButtonNewRisk.append($('#newRiskButtonDiv'));
+            divContainer.append(divButtonNewRisk);
 
-                <tr>
-                  <td>2024</td>
-                  <td>PRESIDÊNCIA</td>
-                  <td>João Batista</td>
-                  <td>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</td>
-                  <td style="color: green; font-weight:bold;">Baixo</td>
-                  <td>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</td>
-                  <td>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</td>
-                </tr> --}}
+            // Move o controle de show entries e a caixa de pesquisa para a direita
+            var divSearchAndEntries = $('<div class="divSearchAndEntries"></div>');
+            divSearchAndEntries.append($('.dataTables_length')).append($('.dataTables_filter'));
+            divContainer.append(divSearchAndEntries);
 
-                </tbody>
-              </table>
-            </div>
-          </div>
+            // Adiciona o contêiner ao topo da tabela
+            $(table.table().container()).prepend(divContainer);
+        }
+    });
 
-        </div>
-        
-      </div>
-      
-    </div>
-    
-  </div>
+    table.on('draw', function(){
+        // Garante que os elementos permaneçam alinhados após a recriação da tabela
+        if (!$(".divContainer").length) {
+            var divContainer = $('<div class="divContainer" style="display: flex; justify-content: space-between;"></div>');
+            
+            var divButtonNewRisk = $('<div class="divButtonNewRisk"></div>');
+            divButtonNewRisk.append($('#newRiskButtonDiv'));
+            divContainer.append(divButtonNewRisk);
 
-  
-  <footer class="rodape">
-    <div class="py-3 ms-3 me-3">
-      <div class="riskLevelDiv">
-        <span>Nível de Risco:</span>
-        <span class="riskLevel1">Baixo</span>
-        <span class="riskLevel2">Médio</span>
-        <span class="riskLevel3">Alto</span>
-    </div>
-  </footer>
-	
-  
-  
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-  <script src="https://cdn.datatables.net/1.10.14/js/jquery.dataTables.min.js"></script>
+            var divSearchAndEntries = $('<div class="divSearchAndEntries"></div>');
+            divSearchAndEntries.append($('.dataTables_length')).append($('.dataTables_filter'));
+            divContainer.append(divSearchAndEntries);
+
+            $(table.table().container()).prepend(divContainer);
+        }
+    });
+});
+
+
+		</script>
+		
+		
+
+
+
 </body>
 </html>
 @endsection
