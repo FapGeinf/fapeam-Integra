03dfeebb91e907bf9604c0118508f3280e21d98c
Leo S. <contato.prof.leonardo@gmail.com>
2024-06-20 00:07:17 -0400

    Correção ortográfica

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 	<meta name="csrf-token" content="{{ csrf_token() }}">
-	<title>{{ config('', 'INTEGRA') }} - @yield('title')</title>
+	<title>{{ config('', 'ÍNTEGRA') }} - @yield('title')</title>
 	@vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 
