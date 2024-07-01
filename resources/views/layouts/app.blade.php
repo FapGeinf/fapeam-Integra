02a069df1ab8119d17647ba8e4cd8b4e44b391b7
Leo S. <contato.prof.leonardo@gmail.com>
2024-07-01 14:20:15 -0400

    Correções

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="shortcut icon" href="{{ asset('img/logoDeconWhiteMin.png') }}">
   <link rel="stylesheet" href="{{asset('css/global.css')}}">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
   rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
@@ -19,12 +19,12 @@
     @if(!Request::is('login'))
     <nav class="navbar">
       @if(Auth::check())
-        <a class="navbar-brand {{ Request::path() === '/' ? 'active' : '' }}" href="{{route('riscos.index')}}">
+        <a class="navbar-brand">
           {{-- <img src="/img/logoDeconWhiteMin.png" alt="logo" width="21" height="20" class="logoNavImg d-inline-block align-text-top"> --}}
           <i class="bi bi-person-workspace iconPerson"></i>
         </a>
         @endif
-        
+
       <div class="navbar-brand">
         @if(Auth::check())
         <div id="navbarDropdown">
@@ -32,12 +32,12 @@
             <span class="userTopNav">Usuário: </span>
             <span class="">{{ Auth::user()->name }}</span>
           </div>
-    
+
           <div>
             <span class="sectorTopNav">Setor: </span>
             <span class="">{{ Auth::user()->unidade->unidadeNome}}</span>
           </div>
-    
+
         </div>
       </div>
 
@@ -52,7 +52,7 @@
         @csrf
       </form>
     </div>
-    
+
     @endif
     </nav>
 
@@ -69,7 +69,7 @@
       </section>
 		</main>
 	</div>
-  
+
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
 </body>
 
