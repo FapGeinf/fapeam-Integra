2219df45ab252d31dd83211117c5765ef3562584
Leo S. <contato.prof.leonardo@gmail.com>
2024-07-05 17:01:06 -0400

    Update index.blade.php

--- resources/views/riscos/index.blade.php
+++ resources/views/riscos/index.blade.php
@@ -50,7 +50,7 @@
 						@foreach ($riscos as $risco)
 							@foreach ($risco->monitoramentos as $monitoramento)
 								<tr style="cursor: pointer;" onclick="window.location='{{ route('riscos.show', $risco->id) }}';">
-									<td style="white-space:nowrap;">{{ $risco->riscoNum }}</td>
+									<td style="white-space:nowrap;">{{ $risco->id }}</td>
 									<td style="white-space: nowrap;">{!! $risco->responsavelRisco !!}</td>
 									<td style="word-wrap:break-word;">{!! $risco->unidade->unidadeNome !!}</td>
 									<td>{!! Str::limit($risco->riscoEvento, 80) !!}</td>
