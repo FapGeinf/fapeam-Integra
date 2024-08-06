90226b4f6151aace7de196b8fd3e160112e03a1d
Leo S. <contato.prof.leonardo@gmail.com>
2024-08-06 17:10:30 -0400

    Página de resposta refeita
    Em vez de parecer uma tabela, agora está parecida com um chat de apps de redes sociais

--- resources/views/riscos/respostas.blade.php
+++ resources/views/riscos/respostas.blade.php
@@ -7,55 +7,76 @@
   <link rel="stylesheet" href="{{asset('css/resp.css')}}">
 </head>
 
-<div class="container-fluid p-30">
+<div class="container p-30">
   <div class="col-12 box-shadow">
     <h4 class="text-center mb-3">Detalhes da Resposta</h4>
     <hr class="line">
 
-    <div class="">
+    <div class="chat-box">
       @if($respostas->count() > 0)
-      <table class="table table-bordered">
-        <tr>
-          <th scope="col" class="text-center thNumber">Respondido por</th>
-          <th scope="col" class="text-center thReply">Resposta do Risco</th>
-          <th scope="col" class="text-center thReply">Ações</th>
-        </tr>
-        @foreach ($respostas as $key => $resposta)
+      @php
+        $lastSetor = null; // Para rastrear o setor da última mensagem
+      @endphp
 
-        <tr>
-          <td class="text-center text13 tdNumber">{{$resposta->user->name}}</td>
-          <td class="text13 tdReply">{!! $resposta->respostaRisco !!}</td>
-          <td class="text-center text13 tdAction">
-            <button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#editRespostaModal" onclick="editResposta({{ $resposta->id }}, '{{ $resposta->respostaRisco }}')">Editar</button>
-          </td>
-        </tr>
+      @foreach ($respostas as $resposta)
+      @php
+        $currentSetor = $resposta->user->unidade->id;
+      @endphp
 
-        <div class="modal fade" id="editRespostaModal" tabindex="-1" aria-labelledby="editRespostaModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title" id="editRespostaModalLabel">Editar Resposta</h5>
-                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                  <form id="editRespostaForm" action="{{route('riscos.updateResposta',['id' => $resposta->id])}}" method="POST">
-                    @csrf
-                    @method('PUT')
-                    <div class="mb-4">
-                      <label for="editRespostaRisco" class="form-label">Resposta</label>
-                      <input type="text" class="form-control" id="editRespostaRisco" name="respostaRisco" required>
-                    </div>
-                    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                      <button type="submit" class="btn btn-success mb-2">Salvar</button>
-                    </div>
-                  </form>
-                </div>
-              </div>
+        @if($lastSetor === $currentSetor)
+          <div class="message other-message {{ $alignmentClass }}">
+            <div class="p-1">
+              <span>
+                <i class="bi bi-person-circle"></i>
+              </span>
+
+              <span>
+                {{ $resposta->user->name }}:
+              </span>
             </div>
-          </div>
+            
+            <hr>
 
-        @endforeach
-      </table>
+            
+            <p class="form-control fStyle" style="background-color: #f0f0f0;">{!! $resposta->respostaRisco !!}</p>
+
+            <div class="text-end">
+              <button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#editRespostaModal" onclick="editResposta({{ $resposta->id }}, '{{ $resposta->respostaRisco }}')">Editar</button>
+            </div>
+
+          </div>
+          
+        @else
+          @php
+            $alignmentClass = (isset($alignmentClass) && $alignmentClass === 'align-left') ? 'align-right' : 'align-left';
+          @endphp
+          <div class="message other-message {{ $alignmentClass }}">
+            <div class="p-1">
+              <span>
+                <i class="bi bi-person-circle"></i>
+              </span>
+
+              <span>
+                {{ $resposta->user->name }}:
+              </span>
+              
+            </div>
+
+            <hr>
+            
+            <p class="form-control fStyle" style="background-color: #f0f0f0;">{!! $resposta->respostaRisco !!}</p>
+
+            <div class="text-end">
+              <button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#editRespostaModal" onclick="editResposta({{ $resposta->id }}, '{{ $resposta->respostaRisco }}')">Editar</button>
+            </div>
+            
+          </div>
+        @endif
+
+        @php
+          $lastSetor = $currentSetor; // Atualiza o setor da última mensagem
+        @endphp
+      @endforeach
       @else
       <p class="text-center">Não há respostas disponíveis para este risco.</p>
       @endif
@@ -93,8 +114,29 @@
   </div>
 </div>
 
-
-
+<div class="modal fade" id="editRespostaModal" tabindex="-1" aria-labelledby="editRespostaModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="editRespostaModalLabel">Editar Resposta</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form id="editRespostaForm" action="{{route('riscos.updateResposta',['id' => $resposta->id])}}" method="POST">
+          @csrf
+          @method('PUT')
+          <div class="mb-4">
+            <label for="editRespostaRisco" class="form-label">Resposta</label>
+            <input type="text" class="form-control" id="editRespostaRisco" name="respostaRisco" required>
+          </div>
+          <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+            <button type="submit" class="btn btn-success mb-2">Salvar</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
 
 <script>
   let respostaCount = 1;
