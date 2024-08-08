b112662599dc94c1d4fbff5e83b393204e598988
Leo S. <contato.prof.leonardo@gmail.com>
2024-08-08 13:17:44 -0400

    50% alteração das cores das mensagens

--- resources/views/riscos/respostas.blade.php
+++ resources/views/riscos/respostas.blade.php
@@ -20,133 +20,136 @@
     <div class="chat-box">
       @if($respostas->count() > 0)
 				@php
-			$lastSetor = null; // Para rastrear o setor da última mensagem
+				  $lastSetor = null; // Para rastrear o setor da última mensagem
+				  $alignmentClass = 'align-left'; // Inicializa a classe de alinhamento
 				@endphp
 
 				@foreach ($respostas as $resposta)
-							  @php
-							$currentSetor = $resposta->user->unidade->id;
-							  @endphp
+				  @php
+				    $currentSetor = $resposta->user->unidade->id;
+				  @endphp
 
-							  @if($lastSetor === $currentSetor)
-								<div class="message other-message {{ $alignmentClass }}">
-								  <div class="p-1">
-                    <div class="d-flex row">
-                      <div class="dataSector">
-                        <div>
-                          Criado em: 
-                          <i class="bi bi-clock"></i>
-                          <span class="dataSpan">
-                            {{ $resposta->created_at->format('d/m/Y') }}
-                            às
-                            {{ $resposta->created_at->format('H:i') }}
-                          </span>
+				  @if($lastSetor === $currentSetor)
+					<div class="message {{ $alignmentClass === 'align-left' ? 'other-message' : 'another-message' }} {{ $alignmentClass }}">
+					  <div class="p-1">
+                        <div class="d-flex row">
+                          <div class="dataSector">
+                            <div>
+                              Criado em: 
+                              <i class="bi bi-clock"></i>
+                              <span class="dataSpan">
+                                {{ $resposta->created_at->format('d/m/Y') }}
+                                às
+                                {{ $resposta->created_at->format('H:i') }}
+                              </span>
+                            </div>
+
+                            <div>
+                              Lotação: 
+                              <i class="bi bi-building"></i>
+                              <span class="dataSpan">
+                              {{ $resposta->user->unidade->unidadeNome }}
+                              </span>
+                            </div>
+
+                            <div>
+                              Perfil: 
+                              <i class="bi bi-person"></i>
+                              <span class="dataSpan">
+                                {{ $resposta->user->name }}
+                              </span>
+                            </div>
+                          </div>
                         </div>
+					  </div>
 
-												<div>
-                          Lotação: 
-                          <i class="bi bi-building"></i>
-                          <span class="dataSpan">
-                          {{ $resposta->user->unidade->unidadeNome }}
-                          </span>
-                        </div>
+					  <hr class="hr2">
 
-                        <div>
-                          Perfil: 
-                          <i class="bi bi-person"></i>
-                          <span class="dataSpan">
-                            {{ $resposta->user->name }}
-                          </span>
-                        </div>
-                      </div>
-                    </div>
-								  </div>
-
-								  <hr class="hr2">
-
-								  <p class="form-control fStyle" style="background-color: #f0f0f0;">{!! $resposta->respostaRisco !!}</p>
-                    <div class="text-end">
-                    <button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#editRespostaModal" onclick="editResposta({{ $resposta->id }}, '{{ $resposta->respostaRisco }}')">
-                      <i class="bi bi-pen"></i>
-                    </button>
-								  </div>
-								</div>
-							  @else
-								@php
-								$alignmentClass = (isset($alignmentClass) && $alignmentClass === 'align-left') ? 'align-right' : 'align-left';
-								@endphp
-
-								<div class="message other-message {{ $alignmentClass }}">
-								  <div class="p-1">
-                    <div class="d-flex row">
-                      <div class="dataSector">
-                        <div>
-                          Criado em:
-                          <i class="bi bi-clock"></i>
-                          <span class="dataSpan">
-                            {{ $resposta->created_at->format('d/m/Y') }}
-                            às
-                            {{ $resposta->created_at->format('H:i') }}
-                          </span>
-                        </div>
-
-												<div>
-                          Lotação: 
-                          <i class="bi bi-building"></i>
-                          <span class="dataSpan">
-                          {{ $resposta->user->unidade->unidadeNome }}
-                          </span>
-                        </div>
-
-                        <div>
-                          Perfil:
-                          <i class="bi bi-person"></i>
-                          <span class="dataSpan">
-                          {{ $resposta->user->name }}
-                          </span>
-                        </div>
-                      </div>
-                    </div>
-								  </div>
-
-								  <hr class="hr2">
-
-								  <p class="form-control fStyle" style="background-color: #f0f0f0;">{!! $resposta->respostaRisco !!}</p>
-
-								  <div class="text-end">
-                    <button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#editRespostaModal" onclick="editResposta({{ $resposta->id }}, '{{ $resposta->respostaRisco }}')">
-                      <i class="bi bi-pen"></i>
-                    </button>
-								  </div>
-								</div>
-							  @endif
-
-							  @php
-							$lastSetor = $currentSetor; // Atualiza o setor da última mensagem
-							  @endphp
-										<div class="modal fade" id="editRespostaModal" tabindex="-1" aria-labelledby="editRespostaModalLabel" aria-hidden="true">
-					  <div class="modal-dialog modal-dialog-centered">
-						<div class="modal-content">
-						  <div class="modal-header">
-							<h5 class="modal-title" id="editRespostaModalLabel">Editar Resposta</h5>
-							<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-						  </div>
-						  <div class="modal-body">
-							<form id="editRespostaForm" action="{{route('riscos.updateResposta', ['id' => $resposta->id])}}" method="POST">
-							  @csrf
-							  @method('PUT')
-							  <div class="mb-4">
-								<label for="editRespostaRisco" class="form-label">Resposta</label>
-								<input type="text" class="form-control" id="editRespostaRisco" name="respostaRisco" required>
-							  </div>
-							  <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-								<button type="submit" class="btn btn-success mb-2">Salvar</button>
-							  </div>
-							</form>
-						  </div>
-						</div>
+					  <p class="form-control fStyle" style="background-color: #f0f0f0;">{!! $resposta->respostaRisco !!}</p>
+                      <div class="text-end">
+                        <button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#editRespostaModal" onclick="editResposta({{ $resposta->id }}, '{{ $resposta->respostaRisco }}')">
+                          <i class="bi bi-pen"></i>
+                        </button>
 					  </div>
 					</div>
+				  @else
+					@php
+					  $alignmentClass = ($alignmentClass === 'align-left') ? 'align-right' : 'align-left';
+					@endphp
+
+					<div class="message {{ $alignmentClass === 'align-left' ? 'other-message' : 'another-message' }} {{ $alignmentClass }}">
+					  <div class="p-1">
+                        <div class="d-flex row">
+                          <div class="dataSector">
+                            <div>
+                              Criado em:
+                              <i class="bi bi-clock"></i>
+                              <span class="dataSpan">
+                                {{ $resposta->created_at->format('d/m/Y') }}
+                                às
+                                {{ $resposta->created_at->format('H:i') }}
+                              </span>
+                            </div>
+
+                            <div>
+                              Lotação: 
+                              <i class="bi bi-building"></i>
+                              <span class="dataSpan">
+                              {{ $resposta->user->unidade->unidadeNome }}
+                              </span>
+                            </div>
+
+                            <div>
+                              Perfil:
+                              <i class="bi bi-person"></i>
+                              <span class="dataSpan">
+                              {{ $resposta->user->name }}
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+					  </div>
+
+					  <hr class="hr2">
+
+					  <p class="form-control fStyle" style="background-color: #f0f0f0;">{!! $resposta->respostaRisco !!}</p>
+
+					  <div class="text-end">
+                        <button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#editRespostaModal" onclick="editResposta({{ $resposta->id }}, '{{ $resposta->respostaRisco }}')">
+                          <i class="bi bi-pen"></i>
+                        </button>
+					  </div>
+					</div>
+				  @endif
+
+				  @php
+				    $lastSetor = $currentSetor; // Atualiza o setor da última mensagem
+				  @endphp
+
+				  <!-- Modal para edição da resposta -->
+				  <div class="modal fade" id="editRespostaModal" tabindex="-1" aria-labelledby="editRespostaModalLabel" aria-hidden="true">
+				    <div class="modal-dialog modal-dialog-centered">
+				      <div class="modal-content">
+				        <div class="modal-header">
+				          <h5 class="modal-title" id="editRespostaModalLabel">Editar Resposta</h5>
+				          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+				        </div>
+				        <div class="modal-body">
+				          <form id="editRespostaForm" action="{{route('riscos.updateResposta', ['id' => $resposta->id])}}" method="POST">
+				            @csrf
+				            @method('PUT')
+				            <div class="mb-4">
+				              <label for="editRespostaRisco" class="form-label">Resposta</label>
+				              <input type="text" class="form-control" id="editRespostaRisco" name="respostaRisco" required>
+				            </div>
+				            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+				              <button type="submit" class="btn btn-success mb-2">Salvar</button>
+				            </div>
+				          </form>
+				        </div>
+				      </div>
+				    </div>
+				  </div>
 
 				@endforeach
 	  @else
@@ -159,6 +162,7 @@
   </div>
 </div>
 
+<!-- Modal para adicionar nova resposta -->
 <div class="modal fade" id="respostaModal" tabindex="-1" aria-labelledby="respostaModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
