05bcf77aa4c0911cd33599a6b5076d608f43629e
LuizFelipe2023 <luizfelipefroisneves@gmail.com>
2024-08-08 12:02:02 -0400

    aplicado o wordcount no store e edit

--- resources/views/riscos/edit.blade.php
+++ resources/views/riscos/edit.blade.php
@@ -2,7 +2,9 @@
 
 @section('content')
 
-@section('title') {{'Editar Formulário'}} @endsection
+@section('title')
+    {{ 'Editar Formulário' }}
+@endsection
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,26 +12,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulário de Risco</title>
-    <link rel="stylesheet" href="{{asset('css/edit.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/edit.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <script src="/ckeditor/ckeditor.js"></script>
 </head>
 
 <body>
-    @if(session('error'))
-    <script>
-        alert('{{ session('
-            error ') }}');
-    </script>
+    @if (session('error'))
+        <script>
+            alert('{{ session('
+                        error ') }}');
+        </script>
     @endif
 
     <div class="error-message alertShow pt-4">
-        @if($errors->any())
-        <div class="alert alert-danger d-flex justify-content-center">
-            @foreach ($errors->all() as $error )
-            <span class="text-center">Houve um erro ao editar esse risco</span>
-            @endforeach
-        </div>
+        @if ($errors->any())
+            <div class="alert alert-danger d-flex justify-content-center">
+                @foreach ($errors->all() as $error)
+                    <span class="text-center">Houve um erro ao editar esse risco</span>
+                @endforeach
+            </div>
         @endif
     </div>
 
@@ -49,94 +51,97 @@
                     <div class="col-sm-4 col-md-4 mQuery">
                         <label for="name">Insira o Ano:</label>
                         <input type="text" id="riscoAno" name="riscoAno" class="form-control dataValue"
-                            value="{{$risco->riscoAno}}" minlength="4" maxlength="4" >
+                            value="{{ $risco->riscoAno }}" minlength="4" maxlength="4">
                     </div>
 
                     <div class="col-sm-8 col-md-8 mQuery">
                         <label for="name">Responsável do Risco:</label>
-                        <input type="text" id="responsavelRisco" name="responsavelRisco" class="form-control dataValue"
-                            value="{{$risco->responsavelRisco ?? old('responsavelRisco')}}" maxlength="100">
+                        <input type="text" id="responsavelRisco" name="responsavelRisco"
+                            class="form-control dataValue"
+                            value="{{ $risco->responsavelRisco ?? old('responsavelRisco') }}" maxlength="100">
                     </div>
                 </div>
 
                 <label id="first" for="riscoEvento">Evento:</label>
-                <textarea name="riscoEvento" class="textInput"
-                    required>{{ $risco->riscoEvento ?? old('riscoEvento') }}</textarea>
+                <textarea name="riscoEvento" class="textInput" required>{{ $risco->riscoEvento ?? old('riscoEvento') }}</textarea>
 
                 <label for="riscoCausa">Causa:</label>
-                <textarea name="riscoCausa" class="textInput"
-                    required>{{ $risco->riscoCausa ?? old('riscoCausa')}}</textarea>
+                <textarea name="riscoCausa" class="textInput" required>{{ $risco->riscoCausa ?? old('riscoCausa') }}</textarea>
 
                 <label for="riscoConsequencia">Consequência:</label>
-                <textarea name="riscoConsequencia" class="textInput"
-                    required>{{ $risco->riscoConsequencia ?? old('riscoConsequencia') }}</textarea>
+                <textarea name="riscoConsequencia" class="textInput" required>{{ $risco->riscoConsequencia ?? old('riscoConsequencia') }}</textarea>
 
 
-                    <div class="row g-3">
-                        <div class="col-sm-3 col-md-3">
-                            <label style="white-space: nowrap;" for="nivel_de_risco">Nivel de Risco:</label>
-                            <select name="nivel_de_risco" id="nivel_de_risco" class="form-select form-control" required>
-                                <option value="1">Baixo</option>
-                                <option value="2">Médio</option>
-                                <option value="3">Alto</option>
-                            </select>
-                        </div>  
+                <div class="row g-3">
+                    <div class="col-sm-3 col-md-3">
+                        <label style="white-space: nowrap;" for="nivel_de_risco">Nivel de Risco:</label>
+                        <select name="nivel_de_risco" id="nivel_de_risco" class="form-select form-control" required>
+                            <option value="1">Baixo</option>
+                            <option value="2">Médio</option>
+                            <option value="3">Alto</option>
+                        </select>
+                    </div>
 
-                        <div class="col-sm-9 col-md-9">
-                            <label for="unidadeId">Unidade:</label>
-                            <select name="unidadeId" class="form-select form-control" required>
-                                <option selected disabled>Selecione uma unidade</option>
-                                @foreach($unidades as $unidade)
+                    <div class="col-sm-9 col-md-9">
+                        <label for="unidadeId">Unidade:</label>
+                        <select name="unidadeId" class="form-select form-control" required>
+                            <option selected disabled>Selecione uma unidade</option>
+                            @foreach ($unidades as $unidade)
                                 <option value="{{ $unidade->id }}"
                                     {{ isset($risco) && $risco->unidadeId == $unidade->id ? 'selected' : '' }}>
                                     {{ $unidade->unidadeNome }}
                                 </option>
-                                @endforeach
-                            </select>
-                        </div>
+                            @endforeach
+                        </select>
                     </div>
+                </div>
 
-                        
 
-                            <hr id="hr5">
 
-                            {{-- <span id="tip">
+                <hr id="hr5">
+
+                {{-- <span id="tip">
                                 <i class="bi bi-exclamation-circle-fill"></i>
                                 Dica: Revise sua edição antes de salvar
                             </span> --}}
 
-                            
-                                <div class="d-flex justify-content-center">
-                                    <div class="me-1">
-                                        <a href="{{ route('riscos.edit-monitoramentos', ['id' => $risco->id]) }}" class="btn btn-primary">Editar Monitoramentos</a>
-                                    </div>
-                                    
-                                    <div>
-                                        <button type="button" onclick="showConfirmationModal()" class="submit-btn">Salvar Edição</button>
-                                    </div>
-                                </div>
 
-                            
+                <div class="d-flex justify-content-center">
+                    <div class="me-1">
+                        <a href="{{ route('riscos.edit-monitoramentos', ['id' => $risco->id]) }}"
+                            class="btn btn-primary">Editar Monitoramentos</a>
+                    </div>
 
-                            <div class="modal fade" id="confirmationModal" tabindex="-1" aria-labelledby="confirmationModalLabel" aria-hidden="true">
-                                <div class="modal-dialog modal-dialog-centered">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h5 class="modal-title" id="confirmationModalLabel">Confirmação de Edição</h5>
-                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                        </div>
-                                        <div class="modal-body">
-                                            <div id="modalContent">
-                                                <!-- Conteúdo do modal será inserido dinamicamente aqui -->
-                                            </div>
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-                                            <button type="button" onclick="submitForm()" class="btn btn-primary">Salvar</button>
-                                        </div>
-                                    </div>
+                    <div>
+                        <button type="button" onclick="showConfirmationModal()" class="submit-btn">Salvar
+                            Edição</button>
+                    </div>
+                </div>
+
+
+
+                <div class="modal fade" id="confirmationModal" tabindex="-1" aria-labelledby="confirmationModalLabel"
+                    aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="confirmationModalLabel">Confirmação de Edição</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <div id="modalContent">
+                                    <!-- Conteúdo do modal será inserido dinamicamente aqui -->
                                 </div>
                             </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary"
+                                    data-bs-dismiss="modal">Cancelar</button>
+                                <button type="button" onclick="submitForm()" class="btn btn-primary">Salvar</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
 
             </form>
@@ -152,7 +157,8 @@
             let riscoCausa = CKEDITOR.instances.riscoCausa.getData();
             let riscoConsequencia = CKEDITOR.instances.riscoConsequencia.getData();
             let nivel_de_risco = document.getElementById('nivel_de_risco').value;
-            let unidadeId = document.querySelector('[name="unidadeId"]').options[document.querySelector('[name="unidadeId"]').selectedIndex].text;
+            let unidadeId = document.querySelector('[name="unidadeId"]').options[document.querySelector(
+                '[name="unidadeId"]').selectedIndex].text;
 
             // Construção do HTML para o modal de confirmação
             let modalContent = `
@@ -213,9 +219,22 @@
             document.getElementById('formCreate').submit();
         }
 
-        CKEDITOR.replace('riscoEvento');
-        CKEDITOR.replace('riscoCausa');
-        CKEDITOR.replace('riscoConsequencia');
+        document.addEventListener("DOMContentLoaded", function() {
+            var ckeditorConfig = {
+                extraPlugins: 'wordcount',
+                wordcount: {
+                    showCharCount: true,
+                    maxCharCount: 10000,
+                    maxCharCountMsg: 'Você atingiu o limite máximo de caracteres permitidos.',
+                    charCountMsg: 'Caracteres restantes: {0}'
+                }
+            };
+            CKEDITOR.replace('riscoEvento',ckeditorConfig);
+            CKEDITOR.replace('riscoCausa',ckeditorConfig);
+            CKEDITOR.replace('riscoConsequencia',ckeditorConfig);
+        });
+
+
     </script>
 
 
