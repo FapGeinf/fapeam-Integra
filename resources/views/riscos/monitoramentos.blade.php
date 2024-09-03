9ad24d4ab2514634027ecad16d899c728de4d52a
Leo S. <contato.prof.leonardo@gmail.com>
2024-09-03 15:41:52 -0400

    aumento do modal

--- resources/views/riscos/monitoramentos.blade.php
+++ resources/views/riscos/monitoramentos.blade.php
@@ -146,7 +146,7 @@
             <!-- Modal de Confirmação -->
             <div class="modal fade" id="confirmationModal" tabindex="-1" aria-labelledby="exampleModalLabel"
                 aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-dialog modal-lg modal-dialog-centered">
                     <div class="modal-content">
                         <div class="modal-header">
                             <h5 class="modal-title" id="exampleModalLabel">Confirmação de Edição</h5>
