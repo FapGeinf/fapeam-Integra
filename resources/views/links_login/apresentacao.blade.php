566cd9fe4f56efce33b01d0287f326e7b469f44f
Leo S. <contato.prof.leonardo@gmail.com>
2024-09-09 15:51:00 -0400

    Esboço inicial da apresentação

--- resources/views/links_login/apresentacao.blade.php
+++ resources/views/links_login/apresentacao.blade.php
@@ -1,1 +1,25 @@
-<h1>Apresentação</h1>
+@extends('layouts.app')
+
+@section('title') {{'Apresentação'}} @endsection
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="{{asset('css/apresentacao.css')}}">
+  <link rel="stylesheet" href="{{asset('css/removeGlobalConfig.css')}}">
+</head>
+
+@section('content')
+<body>
+  <div class="form-wrapper">
+    <div class="form_create">
+      <h1>Apresentação</h1>
+    </div>
+    
+  </div>
+</body>
+@endsection
+</html>
