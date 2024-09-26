c5217db1805abd9d2b1bf3644e2b94c75326b652
LuizFelipe2023 <luizfelipefroisneves@gmail.com>
2024-09-26 14:46:54 -0400

    precisa de ajustes na sidenav...

--- resources/views/sidenav.blade.php
+++ resources/views/sidenav.blade.php
@@ -2,84 +2,88 @@
 <html>
 
 <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="{{asset('css/global.css')}}">
-  <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ asset('css/global.css') }}">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 </head>
 
 <body>
-  <nav class="menu-lateral" id="appSidenav">
-    <a class="navbar-brand">
-      <div class="logo">
-        <img id="" src="{{ asset('img/logoDeconWhiteMin.png') }}" style="height: 60%;  object-fit: cover;">
-      </div>
-    </a>
-
-    <div id="welcome">
-      <p>Bem-vindo(a)!</p>
-    </div>
-
-    <hr class="spacer">
-
-    <ul class="ulList">
-
-      <li class="item-menu">
-        <a href="{{route('riscos.index')}}" class="{{ Request::routeIs('riscos.index') ? 'active' : '' }}" style="margin-top: 1rem;">
-          <i class="bx bi-house icon {{ Request::routeIs('riscos.index') ? 'active-icon' : '' }}"></i>
-          <span class="txt-link">Home</span>
-          {{-- <i class="bx bi-chevron-down dropdown-icon"></i> --}}
+    <nav class="menu-lateral" id="appSidenav">
+        <a class="navbar-brand">
+            <div class="logo">
+                <img id="" src="{{ asset('img/logoDeconWhiteMin.png') }}"
+                    style="height: 60%; object-fit: cover;">
+            </div>
         </a>
-        {{-- <ul class="dropdown">
-          <li class="dropdown-item">Placeholder 1</li>
-          <li class="dropdown-item">Placeholder 2</li>
-        </ul> --}}
-      </li>
 
-      <hr class="spacer">
-
-      <!-- <li class="item-menu">
-        <a href="" class="{{ Request::path() === 'home' ? 'active' : '' }}">
-          <i class='bx bi-file-text icon'></i>
-          <span class="txt-link">Relatórios</span>
-          {{-- <i class="bx bi-chevron-down dropdown-icon"></i> --}}
-        </a>
-        {{-- <ul class="dropdown">
-          <li class="dropdown-item">Placeholder 1</li>
-          <li class="dropdown-item">Placeholder 2</li>
-        </ul> --}}
-      </li> -->
-
-      <li style="display: none" class="item-menu">
-        <a href="{{ route('logout') }}" onclick="event.preventDefault();
-        document.getElementById('logout-form').submit();">
-          <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-            @csrf
-          </form>
-          <i class="bx bxs-exit icon"></i>
-          <span class="txt-link">Sair</span>
-        </a>
-      </li>
-
-      <a href="https://www.fapeam.am.gov.br" target="_blank">
-        <div class="logo bottom">
-          <img id="logoMax" src="{{ asset('img/fapeamLogoMono.png') }}" class="logoImg" alt="Logo">
+        <div id="welcome">
+            <p>Bem-vindo(a)!</p>
         </div>
-      </a>
-    </ul>
-  </nav>
 
-  {{-- <script>
-    document.querySelectorAll('.item-menu > a').forEach(item => {
-      item.addEventListener('click', function (e) {
-        e.preventDefault();
-        const dropdown = this.nextElementSibling;
-        if (dropdown) {
-          dropdown.classList.toggle('show');
-        }
-      });
-    });
-  </script> --}}
+        <hr class="spacer">
+
+        <ul class="ulList">
+            <li class="item-menu">
+                <a href="{{ route('riscos.index') }}" class="{{ Request::routeIs('riscos.index') ? 'active' : '' }}">
+                    <i class="bi bi-house icon"></i>
+                    <span class="txt-link">Home</span>
+                </a>
+            </li>
+
+
+            <hr class="spacer">
+            <li class="item-menu">
+                <a href="{{ route('riscos.implementadas') }}"
+                    class="{{ Request::routeIs('riscos.implementadas') ? 'active' : '' }}">
+                    <i class="bi bi-check-circle icon"></i>
+                    <span class="txt-link">Implementadas</span>
+                </a>
+            </li>
+            <hr class="spacer">
+            <li class="item-menu">
+                <a href="{{ route('riscos.implementadasParcialmente') }}"
+                    class="{{ Request::routeIs('riscos.implementadasParcialmente') ? 'active' : '' }}">
+                    <i class="bi bi-check-circle icon"></i>
+                    <span class="txt-link">Implementadas Parcialmente</span>
+                </a>
+            </li>
+            <hr class="spacer">
+            <li class="item-menu">
+                <a href="{{ route('riscos.emImplementacao') }}"
+                    class="{{ Request::routeIs('riscos.emImplementacao') ? 'active' : '' }}">
+                    <i class="bi bi-hourglass-split icon"></i>
+                    <span class="txt-link">Em Implementação</span>
+                </a>
+            </li>
+            <hr class="spacer">
+            <li class="item-menu">
+                <a href="{{ route('riscos.naoImplementada') }}"
+                    class="{{ Request::routeIs('riscos.naoImplementada') ? 'active' : '' }}">
+                    <i class="bi bi-x-circle icon"></i>
+                    <span class="txt-link">Não Implementada</span>
+                </a>
+            </li>
+            <hr class="spacer">
+
+            <li style="display: none" class="item-menu">
+                <a href="{{ route('logout') }}"
+                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                    <i class="bi bi-door-open icon"></i>
+                    <span class="txt-link">Sair</span>
+                </a>
+            </li>
+
+            <a href="https://www.fapeam.am.gov.br" target="_blank">
+                <div class="logo bottom">
+                    <img id="logoMax" src="{{ asset('img/fapeamLogoMono.png') }}" class="logoImg" alt="Logo">
+                </div>
+            </a>
+        </ul>
+    </nav>
 </body>
 
 </html>
