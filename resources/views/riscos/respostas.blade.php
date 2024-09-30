1c481a1719c7d3752d67d713d9a59b8be8c35265
LuizFelipe2023 <luizfelipefroisneves@gmail.com>
2024-09-30 16:01:45 -0400

    tentando debugar

--- resources/views/riscos/respostas.blade.php
+++ resources/views/riscos/respostas.blade.php
@@ -203,6 +203,31 @@
                     @php
                         $lastSetor = $currentSetor;
                     @endphp
+                    <div class="modal fade" id="deleteAnexoModal" tabindex="-1"
+                        aria-labelledby="deleteAnexoModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="deleteAnexoModalLabel">Confirmar Exclusão do Anexo
+                                    </h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                        aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <p>Tem certeza que deseja excluir o anexo?</p>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary"
+                                        data-bs-dismiss="modal">Cancelar</button>
+                                    <form action="{{ route('riscos.deleteAnexo', $resposta->id) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger">Excluir Anexo</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 @endforeach
             @else
                 <p class="text-center">Não há respostas disponíveis para este risco.</p>
@@ -273,28 +298,7 @@
     </div>
 </div>
 
-<div class="modal fade" id="deleteAnexoModal" tabindex="-1" aria-labelledby="deleteAnexoModalLabel"
-    aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="deleteAnexoModalLabel">Confirmar Exclusão do Anexo</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <p>Tem certeza que deseja excluir o anexo?</p>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-                <form action="{{ route('riscos.deleteAnexo', $resposta->id) }}" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-danger">Excluir Anexo</button>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
+
 
 
 
