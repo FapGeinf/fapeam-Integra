009a78b776e2dead790dde595470df7b8259a78e
Leo S. <contato.prof.leonardo@gmail.com>
2024-10-03 13:34:23 -0400

    Página de eixos BASE finalizada

---
+++ resources/views/eixos.blade.php
@@ -0,0 +1,120 @@
+@extends('layouts.app')
+@section('content')
+
+@section('title') {{ 'Eixos' }} @endsection
+
+<head>
+  <link rel="stylesheet" href="{{ asset('css/eixos.css') }}">
+</head>
+
+<div class="container mt-4">
+  <div class="box-container p-4">
+    <div class="row">
+      <div class="col-md-3 mb-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Eixo 1</h5>
+            <p class="card-text">Descrição do eixo 1.</p>
+            <ul>
+              <li><a href="#">Link 1</a></li>
+              <li><a href="#">Link 2</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3 mb-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Eixo 2</h5>
+            <p class="card-text">Descrição do eixo 2.</p>
+            <ul>
+              <li><a href="#">Link 1</a></li>
+              <li><a href="#">Link 2</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3 mb-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Eixo 3</h5>
+            <p class="card-text">Descrição do eixo 3.</p>
+            <ul>
+              <li><a href="#">Link 1</a></li>
+              <li><a href="#">Link 2</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3 mb-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Eixo 4</h5>
+            <p class="card-text">Descrição do eixo 4.</p>
+            <ul>
+              <li><a href="#">Link 1</a></li>
+              <li><a href="#">Link 2</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3 mb-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Eixo 5</h5>
+            <p class="card-text">Descrição do eixo 5.</p>
+            <ul>
+              <li><a href="#">Link 1</a></li>
+              <li><a href="#">Link 2</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3 mb-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Eixo 6</h5>
+            <p class="card-text">Descrição do eixo 6.</p>
+            <ul>
+              <li><a href="#">Link 1</a></li>
+              <li><a href="#">Link 2</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3 mb-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Eixo 7</h5>
+            <p class="card-text">Descrição do eixo 7.</p>
+            <ul>
+              <li><a href="#">Link 1</a></li>
+              <li><a href="#">Link 2</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3 mb-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Eixo 8</h5>
+            <p class="card-text">Descrição do eixo 8.</p>
+            <ul>
+              <li><a href="#">Link 1</a></li>
+              <li><a href="#">Link 2</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+@endsection
