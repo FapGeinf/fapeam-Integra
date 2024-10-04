37aa28dee3c99fae1660c548e4aba2c215723b65
Leo S. <contato.prof.leonardo@gmail.com>
2024-10-04 16:41:10 -0400

    PÁGINA DE EIXOS FEITA

--- resources/views/eixos.blade.php
+++ resources/views/eixos.blade.php
@@ -7,14 +7,12 @@
   <link rel="stylesheet" href="{{ asset('css/eixos.css') }}">
 </head>
 
-<div class="form-wrapper-title">
+<div class="form-wrapper pt-5">
   <div class="form_create1 border">
-    <div class="row">
-      <div class="titleDiv">
-        <span>
-          Eixos do Programa de Integridade
-        </span>
-      </div>
+    <div class="titleDP text-center">
+      <span>
+        Documentos do Programa de Integridade
+      </span>
     </div>
   </div>
 </div>
@@ -26,9 +24,13 @@
       <div class="col-md-3 mb-4">
         <div class="card border1">
           <div class="card-body">
-            <h5 class="card-title">Eixo 1</h5>
+            <div class="mb-3">
+              <h5 class="card-title">EIXO I</h5>
+              <span>
+                COMPROMETIMENTO E APOIO DA ALTA DIREÇÃO
+              </span>
+            </div>
             
-            <p class="card-text">Descrição do eixo 1.</p>
             <ul>
               <li><a href="#">Apresentação</a></li>
               <li><a href="#">Indicadores</a></li>
@@ -40,8 +42,13 @@
       <div class="col-md-3 mb-4">
         <div class="card border1">
           <div class="card-body">
-            <h5 class="card-title">Eixo 2</h5>
-            <p class="card-text">Descrição do eixo 2.</p>
+            <div class="mb-3">
+              <h5 class="card-title">EIXO II</h5>
+              <span>
+                INSTITUCIONALIZAÇÃO DO CÓDIGO DE CONDUTA
+              </span>
+            </div>
+            
             <ul>
               <li><a href="#">Apresentação</a></li>
               <li><a href="#">Indicadores</a></li>
@@ -53,8 +60,13 @@
       <div class="col-md-3 mb-4">
         <div class="card border1">
           <div class="card-body">
-            <h5 class="card-title">Eixo 3</h5>
-            <p class="card-text">Descrição do eixo 3.</p>
+            <div class="mb-4">
+              <h5 class="card-title">EIXO III</h5>
+              <span>
+                AVALIAÇÃO DE RISCOS
+              </span>
+            </div>
+            
             <ul>
               <li><a href="#">Apresentação</a></li>
               <li><a href="#">Indicadores</a></li>
@@ -66,8 +78,13 @@
       <div class="col-md-3 mb-4">
         <div class="card border1">
           <div class="card-body">
-            <h5 class="card-title">Eixo 4</h5>
-            <p class="card-text">Descrição do eixo 4.</p>
+            <div class="mb-3">
+              <h5 class="card-title">EIXO IV</h5>
+              <span>
+                IMPLEMENTAÇÃO DOS CONTROLES INTERNOS
+              </span>
+            </div>
+            
             <ul>
               <li><a href="#">Apresentação</a></li>
               <li><a href="#">Indicadores</a></li>
@@ -79,8 +96,13 @@
       <div class="col-md-3 mb-4">
         <div class="card border1">
           <div class="card-body">
-            <h5 class="card-title">Eixo 5</h5>
-            <p class="card-text">Descrição do eixo 5.</p>
+            <div class="mb-3">
+              <h5 class="card-title">EIXO V</h5>
+              <span>
+                COMUNICAÇÃO E TREINAMENTOS PERIÓDICOS
+              </span>
+            </div>
+            
             <ul>
               <li><a href="#">Apresentação</a></li>
               <li><a href="#">Indicadores</a></li>
@@ -92,8 +114,13 @@
       <div class="col-md-3 mb-4">
         <div class="card border1">
           <div class="card-body">
-            <h5 class="card-title">Eixo 6</h5>
-            <p class="card-text">Descrição do eixo 6.</p>
+            <div class="mb-3">
+              <h5 class="card-title">EIXO VI</h5>
+              <span>
+                CANAIS DE DENÚNCIA
+              </span>
+            </div>
+            
             <ul>
               <li><a href="#">Apresentação</a></li>
               <li><a href="#">Indicadores</a></li>
@@ -105,8 +132,13 @@
       <div class="col-md-3 mb-4">
         <div class="card border1">
           <div class="card-body">
-            <h5 class="card-title">Eixo 7</h5>
-            <p class="card-text">Descrição do eixo 7.</p>
+            <div class="mb-3">
+              <h5 class="card-title">EIXO VII</h5>
+              <span>
+                INVESTIGAÇÕES INTERNAS
+              </span>
+            </div>
+            
             <ul>
               <li><a href="#">Apresentação</a></li>
               <li><a href="#">Indicadores</a></li>
@@ -118,8 +150,13 @@
       <div class="col-md-3 mb-4">
         <div class="card border1">
           <div class="card-body">
-            <h5 class="card-title">Eixo 8</h5>
-            <p class="card-text">Descrição do eixo 8.</p>
+            <div class="mb-3">
+              <h5 class="card-title">EIXO VIII</h5>
+              <span>
+                MONITORAMENTO CONTÍNUO
+              </span>
+            </div>
+            
             <ul>
               <li><a href="#">Apresentação</a></li>
               <li><a href="{{route('riscos.index')}}">Monitoramentos</a></li>
