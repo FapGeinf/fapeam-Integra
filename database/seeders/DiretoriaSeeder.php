33d31241f1da309a8e33c7318f28a9eca47d8fd9
LuizFelipe2023 <luizfelipefroisneves@gmail.com>
2024-11-06 09:51:54 -0400

    seeders, migrações relacionadas as diretorias foram implementadas.

---
+++ database/seeders/DiretoriaSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Diretoria;
+
+class DiretoriaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Diretoria::factory()->create([
+            'diretoriaSigla' => 'DAF',
+            'diretoriaNome' => 'Diretoria Administrativo-Financeira',
+        ]);
+        Diretoria::factory()->create([
+            'diretoriaSigla' => 'DITEC',
+            'diretoriaNome' => 'Diretoria Técnico-Científica',
+        ]);
+        Diretoria::factory()->create([
+            'diretoriaSigla' => 'GABINETE',
+            'diretoriaNome' => 'Gabinete',
+        ]);
+
+        Diretoria::factory()->create([
+            'diretoriaSigla' => 'DEPLAVI',
+            'diretoriaNome' => 'Departamento de Planejamento e Avaliação Institucional'
+        ]);
+    
+    }
+}
