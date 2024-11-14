c86eec6392d84ddc435b3c48d73fc483a3f8a1f0
Leo S. <contato.prof.leonardo@gmail.com>
2024-11-14 13:59:36 -0400

    Todas as implementações dos filtros estão ok

--- resources/views/riscos/naoImplementada.blade.php
+++ resources/views/riscos/naoImplementada.blade.php
@@ -14,6 +14,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Monitoramentos Implementados</title>
     <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/filterImplement.css') }}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.2.3/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -112,52 +113,80 @@
                     }
                 },
                 initComplete: function() {
-                    // CONTAINER QUE ALINHA TODOS NA MESMA LINHA
-                    var divContainer = $('<div class="divContainer"></div>');
-    
+                    // Cria o dropdown container para os filtros
+                    var dropdownContainer = $(`
+                        <div class="dropdown d-none mb-2">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownFilters" data-bs-toggle="dropdown" aria-expanded="false">
+                                Filtros
+                            </button>
+                            <div class="dropdown-menu p-3" aria-labelledby="dropdownFilters">
+                                <div id="filtersContent"></div>
+                            </div>
+                        </div>
+                    `);
+            
+                    // Verifica e cria o filtro de unidades
                     if (!$('#filterUnidade').length) {
-                        var selectUnidade = $('<select id="filterUnidade" class="form-select form-select-sm divFilterUnidade"><option value="">Todas as Unidades</option></select>');
-                        @foreach ($monitoramentosDaUnidade->unique('risco.unidade.unidadeNome') as $monitoramento)
-                            selectUnidade.append('<option value="{{ $monitoramento->risco->unidade->unidadeNome }}">{{ $monitoramento->risco->unidade->unidadeNome }}</option>');
-                        @endforeach
-    
-                        var labelUnidades = $('<label for="filterUnidade" class="labelUnidade">Unidades:</label>');
-                        $('.dataTables_length').append(labelUnidades).append(selectUnidade);
-                    }
-    
-                    divContainer.append($('.dataTables_filter'));
-                    divContainer.append($('.dataTables_length'));
-                    $(table.table().container()).prepend(divContainer);
-    
-                    $('#filterUnidade').on('change', function() {
-                        var val = $.fn.dataTable.util.escapeRegex($(this).val());
-                        table.column(0).search(val ? '^' + val + '$' : '', true, false).draw();
-                    });
-                }
-            });
-    
-            table.on('draw', function() {
-                if (!$(".divContainer").length) {
-                    var divContainer = $('<div class="divContainer"></div>');
-    
-                    if (!$('#filterUnidade').length) {
-                        var selectUnidade = $('<select id="filterUnidade" class="form-select form-select-sm divFilterUnidade"><option value="">TODAS</option></select>');
+                        var selectUnidade = $('<select style="background-color: #f1f1f1; cursor: pointer;" id="filterUnidade" class="form-select form-select-sm mb-2"><option value="">TODAS</option></select>');
                         @foreach ($monitoramentosDaUnidade->unique('risco.unidade.unidadeNome') as $monitoramento)
                             selectUnidade.append('<option value="{{ $monitoramento->risco->unidade->unidadeSigla }}">{{ $monitoramento->risco->unidade->unidadeSigla }}</option>');
                         @endforeach
-    
-                        var labelUnidades = $('<label for="filterUnidade" class="labelUnidade">Unidades:</label>');
-                        $('.dataTables_length').append(labelUnidades).append(selectUnidade);
+            
+                        var labelUnidades = $('<label for="filterUnidade" class="form-label">Unidades:</label>');
+                        $('#filtersContent').append(labelUnidades).append(selectUnidade);
                     }
-    
-                    divContainer.append($('.dataTables_length'));
-                    divContainer.append($('.dataTables_filter'));
-    
-                    $(table.table().container()).prepend(divContainer);
-    
+            
+                    // Mover a parte de paginação para dentro do dropdown
+                    $('#filtersContent').append($('.dataTables_length'));
+            
+                    // Adiciona o dropdown dentro da div com id "tableHome2_filter"
+                    $('#tableHome2_filter').prepend(dropdownContainer);
+            
+                    // Evento de filtro de unidade (agora filtrando pela sigla)
                     $('#filterUnidade').on('change', function() {
                         var val = $.fn.dataTable.util.escapeRegex($(this).val());
-                        table.column(0).search(val ? '^' + val + '$' : '', true, false).draw();
+                        table.column(0)  // Coluna 0 é onde você deve ter a sigla da unidade
+                            .search(val ? '^' + val + '$' : '', true, false)  // A pesquisa é feita pela sigla exata
+                            .draw();
+                    });
+                }
+            });
+        
+            table.on('draw', function() {
+                // Garante que os filtros sejam movidos para o dropdown após a atualização da tabela
+                if (!$("#dropdownFilters").length) {
+                    var dropdownContainer = $(`
+                        <div class="dropdown mb-2">
+                            <button class="btnFilter dropdown-toggle" type="button" id="dropdownFilters" data-bs-toggle="dropdown" aria-expanded="false">
+                                Filtros
+                            </button>
+                            <div class="dropdown-menu p-3" aria-labelledby="dropdownFilters">
+                                <div id="filtersContent"></div>
+                            </div>
+                        </div>
+                    `);
+            
+                    if (!$('#filterUnidade').length) {
+                        var selectUnidade = $('<select id="filterUnidade" class="form-select form-select-sm mb-2"><option value="">TODAS</option></select>');
+                        @foreach ($monitoramentosDaUnidade->unique('risco.unidade.unidadeNome') as $monitoramento)
+                            selectUnidade.append('<option value="{{ $monitoramento->risco->unidade->unidadeSigla }}">{{ $monitoramento->risco->unidade->unidadeSigla }}</option>');
+                        @endforeach
+            
+                        var labelUnidades = $('<label for="filterUnidade" class="form-label">Unidades:</label>');
+                        $('#filtersContent').append(labelUnidades).append(selectUnidade);
+                    }
+            
+                    $('#filtersContent').append($('.dataTables_length'));
+            
+                    // Adiciona o dropdown dentro da div com id "tableHome2_filter"
+                    $('#tableHome2_filter').prepend(dropdownContainer);
+            
+                    // Evento de filtro de unidade (agora filtrando pela sigla)
+                    $('#filterUnidade').on('change', function() {
+                        var val = $.fn.dataTable.util.escapeRegex($(this).val());
+                        table.column(0)  // Coluna 0 é onde você deve ter a sigla da unidade
+                            .search(val ? '^' + val + '$' : '', true, false)  // A pesquisa é feita pela sigla exata
+                            .draw();
                     });
                 }
             });
