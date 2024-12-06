bb7975364f50f5064203e3492e5ea9b522bcec79
Leo S. <contato.prof.leonardo@gmail.com>
2024-12-06 15:32:14 -0400

    Correções

--- resources/views/links_login/newHome.blade.php
+++ resources/views/links_login/newHome.blade.php
@@ -5,9 +5,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="{{asset('css/home.css')}}">
+  <link rel="shortcut icon" href="{{ asset('img/logoDeconWhiteMin.png') }}">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <title>Document</title>
+  <title>Íntegra | Início</title>
 </head>
 
 <body class="login-page">
