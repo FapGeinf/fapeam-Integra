83566bf03a7328fd5ae2f5205578ff8065b379f5
LuizFelipe2023 <luizfelipefroisneves@gmail.com>
2024-12-11 15:29:42 -0400

    faltava umas cdn para funcionar os download de pdf, csv e png

--- resources/views/graficos/index.blade.php
+++ resources/views/graficos/index.blade.php
@@ -60,6 +60,8 @@
 </div>
 <script src="https://code.highcharts.com/highcharts.js"></script>
 <script src="https://code.highcharts.com/modules/exporting.js"></script>
+<script src="https://code.highcharts.com/modules/offline-exporting.js"></script>
+<script src="https://code.highcharts.com/modules/export-data.js"></script>
 <script>
     document.addEventListener('DOMContentLoaded', function () {
         Highcharts.chart('grafico-atividades', {
