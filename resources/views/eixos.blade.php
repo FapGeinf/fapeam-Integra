909622086fe5586a8447b73bd9170aea88d6029f
Leo S. <contato.prof.leonardo@gmail.com>
2024-12-11 16:37:17 -0400

    base PRONTA
    faltando apenas inserir os links funcionais em cada botão

--- resources/views/eixos.blade.php
+++ resources/views/eixos.blade.php
@@ -9,17 +9,17 @@
 </head>
 
 <div class="form-wrapper pt-5">
-  <div class="form_create1 border">
-    <div class="titleDP text-center">
-      <span>
-        Eixos do Programa de Integridade
+  <div class="form_create1">
+    <div class="text-center">
+      <span class="custom__span-title fw-bold">
+        EIXOS DO PROGRAMA DE INTEGRIDADE
       </span>
     </div>
   </div>
 </div>
 
 <div class="form-wrapper pt-4">
-  <div class="form_create border">
+  <div class="custom__form_create">
     <div class="row">
 
       <!-- EIXO I -->
@@ -30,7 +30,7 @@
               EIXO I
             </div>
 
-            <div class="mt-5">
+            <div class="mt-5 custom__mouse">
               <span style="color: #0c326f">
                 <i class="fas fa-mouse-pointer"></i>
                 Passe o mouse
@@ -39,7 +39,6 @@
 
           </div>
 
-          <!-- Card alternativo -->
           <div class="card-hover">
             <p class="p-5">COMPROMETIMENTO E APOIO DA ALTA DIREÇÃO</p>
             <button class="btn btn-light mt-5 mb-3">Ler mais</button>
@@ -55,14 +54,14 @@
               EIXO II
             </div>
 
-            <div class="mt-5">
+            <div class="mt-5 custom__mouse">
               <span style="color: #0c326f">
                 <i class="fas fa-mouse-pointer"></i>
                 Passe o mouse
               </span>
             </div>
           </div>
-          <!-- Card alternativo -->
+
           <div class="card-hover">
             <p class="p-5">INSTITUCIONALIZAÇÃO DO CÓDIGO DE CONDUTA</p>
             <button class="btn btn-light mt-5 mb-3">Ler mais</button>
@@ -78,14 +77,14 @@
               EIXO III
             </div>
 
-            <div class="mt-5">
+            <div class="mt-5 custom__mouse">
               <span style="color: #0c326f">
                 <i class="fas fa-mouse-pointer"></i>
                 Passe o mouse
               </span>
             </div>
           </div>
-          <!-- Card alternativo -->
+
           <div class="card-hover">
             <p class="p-5">AVALIAÇÃO DE RISCO</p>
             <button class="btn btn-light mt-5 mb-3">Ler mais</button>
@@ -93,24 +92,117 @@
         </div>
       </div>
 
-      <!-- EIXO IIII -->
+      <!-- EIXO IV -->
       <div class="col-md-3 mb-4">
         <div class="card overflow-hidden">
           <div class="card-body card-fofinho p-4">
             <div class="mb-3 title-fofinho d-flex">
-              EIXO IIII
+              EIXO IV
             </div>
 
-            <div class="mt-5">
+            <div class="mt-5 custom__mouse">
               <span style="color: #0c326f">
                 <i class="fas fa-mouse-pointer"></i>
                 Passe o mouse
               </span>
             </div>
           </div>
-          <!-- Card alternativo -->
+
           <div class="card-hover">
             <p class="p-5">IMPLEMENTAÇÃO DOS CONTROLES INTERNOS</p>
+            <button class="btn btn-light mt-5 mb-3">Ler mais</button>
+          </div>
+        </div>
+      </div>
+
+      <!-- EIXO V -->
+      <div class="col-md-3 mb-4">
+        <div class="card overflow-hidden">
+          <div class="card-body card-fofinho p-4">
+            <div class="mb-3 title-fofinho d-flex">
+              EIXO V
+            </div>
+
+            <div class="mt-5 custom__mouse">
+              <span style="color: #0c326f">
+                <i class="fas fa-mouse-pointer"></i>
+                Passe o mouse
+              </span>
+            </div>
+
+          </div>
+
+          <div class="card-hover">
+            <p class="p-5">COMUNICAÇÃO E TREINAMENTOS PERIÓDICOS</p>
+            <button class="btn btn-light mt-5 mb-3">Ler mais</button>
+          </div>
+        </div>
+      </div>
+
+      <!-- EIXO VI -->
+      <div class="col-md-3 mb-4">
+        <div class="card overflow-hidden">
+          <div class="card-body card-fofinho p-4">
+            <div class="mb-3 title-fofinho d-flex">
+              EIXO VI
+            </div>
+
+            <div class="mt-5 custom__mouse">
+              <span style="color: #0c326f">
+                <i class="fas fa-mouse-pointer"></i>
+                Passe o mouse
+              </span>
+            </div>
+          </div>
+
+          <div class="card-hover">
+            <p class="p-5">CANAIS DE DENÚNCIA</p>
+            <button class="btn btn-light mt-5 mb-3">Ler mais</button>
+          </div>
+        </div>
+      </div>
+
+      <!-- EIXO VII -->
+      <div class="col-md-3 mb-4">
+        <div class="card overflow-hidden">
+          <div class="card-body card-fofinho p-4">
+            <div class="mb-3 title-fofinho d-flex">
+              EIXO VII
+            </div>
+
+            <div class="mt-5 custom__mouse">
+              <span style="color: #0c326f">
+                <i class="fas fa-mouse-pointer"></i>
+                Passe o mouse
+              </span>
+            </div>
+          </div>
+
+          <div class="card-hover">
+            <p class="p-5">INVESTIGAÇÕES INTERNAS</p>
+            <button class="btn btn-light mt-5 mb-3">Ler mais</button>
+          </div>
+        </div>
+      </div>
+
+      <!-- EIXO VIII -->
+      <div class="col-md-3 mb-4">
+        <div class="card overflow-hidden">
+          <div class="card-body card-fofinho p-4">
+            <div class="mb-3 title-fofinho d-flex">
+              EIXO VIII
+            </div>
+
+            <div class="mt-5 custom__mouse">
+              <span style="color: #0c326f">
+                <i class="fas fa-mouse-pointer"></i>
+                Passe o mouse
+              </span>
+            </div>
+          </div>
+
+          <div class="card-hover">
+            <p class="p-5">MONITORAMENTO CONTÍNUO</p>
             <button class="btn btn-light mt-5 mb-3">Ler mais</button>
           </div>
         </div>
