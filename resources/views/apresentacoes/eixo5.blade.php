4f5076d503a67cd58de10098442e8803f13edf43
Carlos Bruno Bonfim <33705093+CarlosBonfim@users.noreply.github.com>
2024-12-11 10:56:52 -0400

    Alteração no texto teste e nos botões das apresentações dos eixos

--- resources/views/apresentacoes/eixo5.blade.php
+++ resources/views/apresentacoes/eixo5.blade.php
@@ -28,7 +28,7 @@
     <div class="form_create border">
       <div class="titleDP text-center fw-bold">
         <span>
-          Comunicação e Treinamentos Periódicos
+          Eixo V - Comunicação e Treinamentos Periódicos
         </span>
       </div>
     </div>
@@ -37,23 +37,13 @@
   <div class="form-wrapper pt-3">
     <div class="form_create border">
       <div class="textDP text__justify">
-        <span>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae quibusdam, nisi, tempore corporis quis
-          possimus dignissimos debitis vero labore voluptatem ducimus consequatur maxime culpa! Officiis aperiam
-          necessitatibus aut iste, exercitationem at voluptatibus eaque provident odit sapiente, illum animi unde
-          repellat et modi asperiores sit est reiciendis ut rerum fuga harum. Nostrum quos quasi obcaecati dolor, enim
-          similique. Expedita, est voluptas. Rerum dolorem, provident cum suscipit expedita exercitationem voluptatum
-          molestias sint, voluptate blanditiis consequatur assumenda cumque non omnis recusandae aspernatur aut vero
-          mollitia vel accusamus. Numquam, tempora laudantium accusamus ea adipisci unde, possimus similique libero
-          nesciunt minus dicta nulla explicabo totam provident consequatur odit voluptates corrupti quasi officiis
-          sapiente. Ducimus veniam corrupti, maxime suscipit fugiat dolor unde cupiditate neque quas veritatis, amet
-          minima! Facilis iure aliquid ad, dicta dignissimos harum similique, perferendis alias obcaecati illo numquam
-          error deserunt fuga. Veniam sequi voluptas alias obcaecati enim mollitia iusto sint cumque similique,
-          voluptate est reiciendis doloremque animi id assumenda cupiditate? Veritatis itaque autem sunt inventore natus
-          veniam quam quo adipisci eligendi dolorum dicta velit corrupti necessitatibus voluptatum non earum beatae
-          debitis esse quas, quos in dolor excepturi illum! Tempora consectetur qui quibusdam dolore? Perspiciatis
-          doloribus odit explicabo aperiam culpa vel, dignissimos molestiae molestias?
-        </span>
+			<span>
+  			<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae quibusdam, nisi, tempore corporis quis possimus dignissimos debitis vero labore voluptatem ducimus consequatur maxime culpa! Officiis aperiam necessitatibus aut iste, exercitationem at voluptatibus eaque provident odit sapiente, illum animi unde repellat et modi asperiores sit est reiciendis ut rerum fuga harum.</p>
+  			<p>Nostrum quos quasi obcaecati dolor, enim similique. Expedita, est voluptas. Rerum dolorem, provident cum suscipit expedita exercitationem voluptatum molestias sint, voluptate blanditiis consequatur assumenda cumque non omnis recusandae aspernatur aut vero mollitia vel accusamus. Numquam, tempora laudantium accusamus ea adipisci unde, possimus similique libero nesciunt minus dicta nulla explicabo totam provident consequatur odit voluptates corrupti quasi officiis sapiente.</p>
+  			<p>Ducimus veniam corrupti, maxime suscipit fugiat dolor unde cupiditate neque quas veritatis, amet minima! Facilis iure aliquid ad, dicta dignissimos harum similique, perferendis alias obcaecati illo numquam error deserunt fuga.</p>
+  			<p>Veniam sequi voluptas alias obcaecati enim mollitia iusto sint cumque similique, voluptate est reiciendis doloremque animi id assumenda cupiditate? Veritatis itaque autem sunt inventore natus veniam quam quo adipisci eligendi dolorum dicta velit corrupti necessitatibus voluptatum non earum beatae debitis esse quas, quos in dolor excepturi illum!</p>
+  			<p>Tempora consectetur qui quibusdam dolore? Perspiciatis doloribus odit explicabo aperiam culpa vel, dignissimos molestiae molestias?</p>
+			</span>
       </div>
     </div>
   </div>
@@ -62,6 +52,7 @@
     <a href="{{ route('atividades.index', ['eixo_id' => 5]) }}">
       <button class="btn__bg btn__bg_color shadow-sm fw-bold text-decoration-none text-center">Atividades</button>
     </a>
+		<button class="btn__bg btn__bg_color shadow-sm fw-bold text-decoration-none text-center">Relatórios</button>
   </div>
 
 </body>
