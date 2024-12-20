54fde2e02845a7ac09b11d0ad99c9038daeecbd7
Carlos Bruno Bonfim <33705093+CarlosBonfim@users.noreply.github.com>
2024-12-20 14:55:56 -0400

    alteração do tipo

--- database/seeders/CanalSeeder.php
+++ database/seeders/CanalSeeder.php
@@ -24,7 +24,7 @@ class CanalSeeder extends Seeder
            ]);
 
            Canal::factory()->create([
-             "nome" => "Instagram"
+             "nome" => "Memorando"
            ]);
     }
 }
