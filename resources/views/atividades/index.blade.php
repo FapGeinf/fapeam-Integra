fd4bafaaee093e73c75b785422e40b7991d1feab
Carlos Bruno Bonfim <33705093+CarlosBonfim@users.noreply.github.com>
2025-01-24 12:22:44 -0400

    Alteração tabela, index da atividade
    Essas alterações  foram necessarias, pra comportar a criação do campo responsável da atividade

--- resources/views/atividades/index.blade.php
+++ resources/views/atividades/index.blade.php
@@ -72,84 +72,98 @@
               <th scope="col">Público Alvo</th>
               <th scope="col">Tipo de Evento</th>
               <th scope="col">Canal de Divulgação</th>
-              <th scope="col">Data Prevista</th>
-              <th scope="col">Data Realizada</th>
+              <th scope="col">Datas</th>
+              <!-- <th scope="col">Data Realizada</th> -->
               <th scope="col">Meta</th>
-              <th scope="col">Realizado</th>
+              <!-- <th scope="col">Realizado</th> -->
               <th scope="col">Ações</th>
             </tr>
           </thead>
           <tbody>
             @foreach ($atividades as $atividade)
-            <tr>
-              <td style="text-align:center;">
-                @foreach ($atividade->eixos as $eixo)
-                <span class="badge bg-primary">{{ $eixo->nome }}</span>
-                @endforeach
-              </td>
-              <td style="text-align: center">{!! $atividade->atividade_descricao !!}</td>
-              <td class="text-center">{!! $atividade->objetivo !!}</td>
-							<td style="text-align: center;">{!! $atividade->responsavel !!}</td>
-              <!-- Exibe o nome correspondente ao id do público alvo -->
-              <td class="text-center">
-                {{ $atividade->publico->nome ?? 'Não informado' }}
-              </td>
-              <td class="text-center">
-								@if($atividade->tipo_evento == 1)
-    							Presencial
-								@elseif($atividade->tipo_evento == 0 || $atividade->tipo_evento === null)
-    							Sem evento
-								@else
-    							Online
+							<tr>
+							  <td style="text-align:center;">
+								@foreach ($atividade->eixos as $eixo)
+								<span class="badge bg-primary">{{ $eixo->nome }}</span>
+								@endforeach
+							  </td>
+							  <td style="text-align: center">{!! $atividade->atividade_descricao !!}</td>
+							  <td class="text-center">{!! $atividade->objetivo !!}</td>
+											<td style="text-align: center;">{!! $atividade->responsavel !!}</td>
+							  <!-- Exibe o nome correspondente ao id do público alvo -->
+							  <td class="text-center">
+								{{ $atividade->publico->nome ?? 'Não informado' }}
+							  </td>
+							  <td class="text-center">
+												@if($atividade->tipo_evento == 1)
+												Presencial
+												@elseif($atividade->tipo_evento == 0 || $atividade->tipo_evento === null)
+												Sem evento
+												@else
+												Online
+												@endif
+							  </td>
+							  <td class="text-center">
+								@foreach ($atividade->canais as $canal)
+								<span class="">{{ $canal->nome }}</span>
+								@endforeach
+							  </td>
+
+								<td class="text-center">
+    							<div class="mt-2">
+        						<div class="text-muted">Data Prevista</div>
+        						<div>{{ \Carbon\Carbon::parse($atividade->data_prevista)->format('d/m/Y') }}</div>
+    							</div>
+    							<div class="mt-2">
+        						<div class="text-muted">Data Realizada</div>
+        						<div>{{ $atividade->data_realizada ? \Carbon\Carbon::parse($atividade->data_realizada)->format('d/m/Y') : 'Não realizada' }}</div>
+    							</div>
+								</td>
+
+								<td class="text-center">
+    							<div class="mt-2">
+       							<div class="text-muted">Meta</div>
+        						<div>{{$atividade->meta}} {{$atividade->medida->nome ?? 'N/A'}}</div>
+    							</div>
+    							<div class="mt-2">
+        						<div class="text-muted">Realizado</div>
+        						<div>{{$atividade->realizado}} {{$atividade->medida->nome ?? 'N/A'}}</div>
+    							</div>
+								</td>
+
+
+							  <td>
+								@if(Auth::user()->unidade->unidadeTipoFK == 1)
+								<div class="d-flex justify-content-start">
+								  <a href="{{ route('atividades.edit', $atividade->id) }}" class="btn btn-sm btn-warning me-2"><i class="bi bi-pencil"></i></a>
+								  <button type="button" class="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#deleteModal{{ $atividade->id }}"><i class="bi bi-trash"></i></button>
+								</div>
 								@endif
-              </td>
-              <td class="text-center">
-                @foreach ($atividade->canais as $canal)
-                <span class="">{{ $canal->nome }}</span>
-                @endforeach
-              </td>
-              <td class="text-center">{{ \Carbon\Carbon::parse($atividade->data_prevista)->format('d/m/Y') }}</td>
-              <td class="text-center">
-                {{ $atividade->data_realizada ? \Carbon\Carbon::parse($atividade->data_realizada)->format('d/m/Y') : 'Não realizada' }}
-              </td>
-              <td class="text-center">
-                {{$atividade->meta}} {{$atividade->medida->nome ?? 'N/A'}}
-              </td>
-              <td class="text-center">
-                {{$atividade->realizado}} {{$atividade->medida->nome ?? 'N/A'}}
-              </td>
-              <td>
-                @if(Auth::user()->unidade->unidadeTipoFK == 1)
-                <div class="d-flex justify-content-start">
-                  <a href="{{ route('atividades.edit', $atividade->id) }}" class="btn btn-sm btn-warning me-2"><i class="bi bi-pencil"></i></a>
-                  <button type="button" class="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#deleteModal{{ $atividade->id }}"><i class="bi bi-trash"></i></button>
-                </div>
-                @endif
-              </td>
-            </tr>
-            <!-- Modal de Confirmação de Exclusão -->
-            <div class="modal fade" id="deleteModal{{ $atividade->id }}" tabindex="-1" aria-labelledby="deleteModalLabel{{ $atividade->id }}" aria-hidden="true">
-              <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h5 class="modal-title" id="deleteModalLabel{{ $atividade->id }}">Confirmar Exclusão</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                  </div>
-                  <div class="modal-body">
-                    Tem certeza que deseja excluir esta atividade?
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="secondary" data-bs-dismiss="modal">Cancelar</button>
-                    <form action="{{ route('atividades.delete', $atividade->id) }}" method="POST">
-                      @csrf
-                      @method('DELETE')
-                      <button type="submit" class="btn btn-danger">Excluir</button>
-                    </form>
-                  </div>
-                </div>
-              </div>
-            </div>
-            @endforeach
+							  </td>
+							</tr>
+							<!-- Modal de Confirmação de Exclusão -->
+							<div class="modal fade" id="deleteModal{{ $atividade->id }}" tabindex="-1" aria-labelledby="deleteModalLabel{{ $atividade->id }}" aria-hidden="true">
+							  <div class="modal-dialog modal-dialog-centered">
+								<div class="modal-content">
+								  <div class="modal-header">
+									<h5 class="modal-title" id="deleteModalLabel{{ $atividade->id }}">Confirmar Exclusão</h5>
+									<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+								  </div>
+								  <div class="modal-body">
+									Tem certeza que deseja excluir esta atividade?
+								  </div>
+								  <div class="modal-footer">
+									<button type="button" class="secondary" data-bs-dismiss="modal">Cancelar</button>
+									<form action="{{ route('atividades.delete', $atividade->id) }}" method="POST">
+									  @csrf
+									  @method('DELETE')
+									  <button type="submit" class="btn btn-danger">Excluir</button>
+									</form>
+								  </div>
+								</div>
+							  </div>
+							</div>
+			@endforeach
           </tbody>
         </table>
       </div>
