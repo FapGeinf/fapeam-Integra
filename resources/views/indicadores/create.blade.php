1ad670bb3d547fbb32bc8109534c5b37e9aa0beb
Leo S. <contato.prof.leonardo@gmail.com>
2025-02-07 09:37:12 -0400

    padronização

--- resources/views/indicadores/create.blade.php
+++ resources/views/indicadores/create.blade.php
@@ -1,28 +1,60 @@
 @extends('layouts.app')
 
+@section('title') {{ 'Criar Indicadores' }} @endsection
+
 @section('content')
-<div class="container">
-	<h1>Criar Indicador</h1>
-	<form action="{{ route('indicadores.store') }}" method="POST">
-		@csrf
-		<div class="form-group">
-			<label for="eixo">Eixo</label>
-			<select class="form-control" id="eixo" name="eixo" required>
-				@foreach($eixos as $eixo)
-					<option value="{{ $eixo->id }}">{{ $eixo->nome }}</option>
-				@endforeach
-			</select>
-		</div>
-		<div class="form-group">
-			<label for="nome">Titulo</label>
-			<input type="text" class="form-control" id="nome" name="nome" required>
-		</div>
-		<div class="form-group">
-			<label for="descricao">Descrição</label>
-			<textarea class="form-control" id="descricao" name="descricao" rows="3" required></textarea>
-		</div>
-		<button type="submit" class="btn btn-primary">Salvar</button>
-	</form>
-</div>
+
+<link rel="stylesheet" href="{{ asset('css/edit.css') }}">
+
+<style>
+	.form-label {
+		margin-bottom: 0 !important;
+	}
+
+	.liDP {
+		margin-left: 0 !important;
+	}
+</style>
+
+<div class="form-wrapper paddingLeft">
+	<div class="form_create border">
+		<h3 style="text-align: center; margin-bottom: 5px;">
+      Criar Indicador
+    </h3>
+
+		<div class="tipWarning mb-3">
+      <span class="asteriscoTop">*</span>
+      Campos obrigatórios
+    </div>
+
+		<form action="{{ route('indicadores.store') }}" method="POST">
+			@csrf
+
+			<div class="row">
+				<div class="col-12">
+					<label for="eixo" class="form-label">Eixo:</label>
+					<select class="form-select" id="eixo" name="eixo" required>
+						@foreach($eixos as $eixo)
+							<option value="{{ $eixo->id }}">{{ $eixo->nome }}</option>
+						@endforeach
+					</select>
+				</div>
+
+				<div class="col-12">
+					<label for="nome" class="form-label">Título:</label>
+					<input type="text" class="form-control" id="nome" name="nome" required>
+				</div>
+
+				<div class="col-12">
+					<label for="descricao">Descrição:</label>
+					<textarea	textarea class="form-control" id="descricao" name="descricao" rows="3" required></textarea>
+				</div>
+			</div>
+
+			<div class="d-flex justify-content-end mt-3">
+				<button type="submit" class="blue-btn me-0">Salvar</button>
+			</div>
+		</form>
+	</div>
 </div>
 @endsection
