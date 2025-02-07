62f48adcb57ce0c802360c4389458629c92cee4d
Leo S. <contato.prof.leonardo@gmail.com>
2025-02-07 09:07:20 -0400

    padronização

--- resources/views/indicadores/index.blade.php
+++ resources/views/indicadores/index.blade.php
@@ -1,43 +1,53 @@
-
 @extends('layouts.app')
 @section('content')
-@section('title') {{ 'Lista de Atividades' }} @endsection
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Indicadores</title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-</head>
-<body>
-	<div class="container mt-5">
-		<h2 class="mb-4">Indicadores</h2>
-		<table class="table table-bordered">
-			<thead>
-				<tr>
-					<th>N°</th>
-					<th>Nome</th>
-					<th>Descrição</th>
-					<th>Eixo</th>
-					<th>Edição</th>
-				</tr>
-			</thead>
-			<tbody>
-				@foreach($indicadores as $indicador)
-				<tr>
-					<td>{{ $indicador->id }}</td>
-					<td>{{ $indicador->nomeIndicador }}</td>
-					<td>{{ $indicador->descricaoIndicador }}</td>
-					<td>EIXO {{$indicador->eixo->id}} - {{ $indicador->eixo->nome}}</td>
-					<td>
-						<a href="{{ route('indicadores.edit', $indicador->id) }}" class="btn btn-warning btn-sm">Editar</a>
-					</td>
-				</tr>
-				@endforeach
-			</tbody>
-		</table>
-		<a href="{{ route('indicadores.create') }}" class="btn btn-primary">Adicionar Indicador</a>
+@section('title') {{ 'Lista de Indicadores' }} @endsection
+
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+<link rel="stylesheet" href="{{ asset('css/atividades.css') }}">
+
+<div class="container-fluid mt-5 px__custom">
+  <div class="col-12 border main-datatable">
+    <div class="d-flex justify-content-center text-center p-2" style="flex-direction: column;">
+      <span style="font-size:22px;">Indicadores</span>
+    </div>
+  </div>
+</div>
+
+<div class="container-fluid p-30">
+  <div class="col-12 border main-datatable">
+		<div class="container-fluid pt-4 pb-4">
+			<div class="mb-4 text-end">
+				<a href="{{ route('indicadores.create') }}" class="primary">Adicionar Indicador</a>
+			</div>
+			
+			<table class="table table-bordered table-striped">
+				<thead>
+					<tr>
+						<th class="text-center">N°</th>
+						<th class="text-center">Nome</th>
+						<th class="text-center">Descrição</th>
+						<th class="text-center">Eixo</th>
+						<th class="text-center">Ações</th>
+					</tr>
+				</thead>
+				<tbody>
+					@foreach($indicadores as $indicador)
+					<tr>
+						<td>{{ $indicador->id }}</td>
+						<td>{{ $indicador->nomeIndicador }}</td>
+						<td>{{ $indicador->descricaoIndicador }}</td>
+						<td>EIXO {{$indicador->eixo->id}} - {{ $indicador->eixo->nome}}</td>
+						<td>
+							<a href="{{ route('indicadores.edit', $indicador->id) }}" class="btn btn-warning btn-sm">Editar</a>
+						</td>
+					</tr>
+					@endforeach
+				</tbody>
+			</table>
+			
+		</div>
 	</div>
-</body>
-</html>
+</div>
+
+
 @endsection
