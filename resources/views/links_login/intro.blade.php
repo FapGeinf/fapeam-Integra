9b61b06bfac36ef81ec0b145e0b6c09d79416ae9
Leo S. <contato.prof.leonardo@gmail.com>
2025-02-21 14:30:36 -0400

    bug fix

--- resources/views/links_login/intro.blade.php
+++ resources/views/links_login/intro.blade.php
@@ -1,18 +1,10 @@
 @extends('layouts.app')
 @section('content')
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="{{asset('css/home.css')}}">
-  <link rel="shortcut icon" href="{{ asset('img/logoDeconWhiteMin.png') }}">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <title>Íntegra | Início</title>
-</head>
+@section('title') {{ 'Início' }} @endsection
+
+<link rel="stylesheet" href="{{asset('css/home.css')}}">
+<link rel="shortcut icon" href="{{ asset('img/logoDeconWhiteMin.png') }}">
 
 <body class="login-page">
 
