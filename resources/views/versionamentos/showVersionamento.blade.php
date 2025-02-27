a1bfa5da0a273cfe5d677ac4851204a241dc1298
LuizFelipe2023 <luizfelipefroisneves@gmail.com>
2025-02-27 14:01:48 -0400

    alterando o show do versionamento.

--- resources/views/versionamentos/showVersionamento.blade.php
+++ resources/views/versionamentos/showVersionamento.blade.php
@@ -1,46 +1,66 @@
 @extends('layouts.app')
-
 @section('content')
-@php
-    \Carbon\Carbon::setLocale('pt_BR'); // Define a localidade para português do Brasil
-@endphp
-
-<link rel="stylesheet" href="{{ asset('css/detalhesAtividade.css') }}">
-
-@section('title') {{ 'Versionamentos' }} @endsection
-
-<div class="margin-30">
-    <div class="box margin-top-4 bg-white border-bottom-none mx-auto">
-        <div class="row mt-3">
-            <div class="text-center">
-                <h3>Últimos Versionamentos</h3>
-            </div>
-            <div class="col-12 text-center fs-18">
-                <table class="table table-bordered table-sm">
-                    <thead>
-                        <tr>
-                            <th scope="col">Título</th>
-                            <th scope="col">Descrição</th>
-                            <th scope="col">Data</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($versionamentos as $versionamento)
+<link href="{{ asset('css/dataTables.dataTables.min.css') }}" rel="stylesheet" />
+<script src="{{ asset('js/jquery-3.7.1.min.js') }}"></script>
+<script src="{{ asset('js/dataTables.min.js') }}"></script>
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+    
+<div class="container-xl vh-100 d-flex align-items-center justify-content-center mt-5 mb-5">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card rounded-2 shadow-sm border-1">
+                <div class="card-header">
+                    <h2 class="fw-bold text-center mt-2 mb-2">Versionamentos</h2>
+                </div>
+                <div class="row g-3 gap-3 px-3 py-2 mb-4">
+                    <div class="col-md-6">
+                        <label for="filter-data" class="fw-bold">Ordenar por Data Prevista</label>
+                        <select name="filter-data" id="filter-data" class="form-select pointer">
+                            <option value="asc">Mais Antiga</option>
+                            <option value="desc">Mais Recente</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="card-body">
+                    @if (session('warning'))
+                        <div class="alert alert-warning">
+                            {{ session('warning') }}
+                        </div>
+                    @endif
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+                    @if (session('success'))
+                        <div class="alert alert-success">
+                            {{ session('success') }}
+                        </div>
+                    @endif
+                    <table class="table table-striped mt-3 mb-3" id="versionamentosTable">
+                        <thead>
                             <tr>
-                                <td>{{ $versionamento->titulo }}</td>
-                                <td>{{ $versionamento->descricao }}</td>
-                                <td>{{ \Carbon\Carbon::parse($versionamento->created_at)->translatedFormat('d \d\e F \d\e Y') }}</td>
+                                <th>Título</th>
+                                <th>Descrição</th>
+                                <th>Data</th>
                             </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            @foreach ($versionamentos as $versionamento)
+                                <tr>
+                                    <td>{{ $versionamento->titulo }}</td>
+                                    <td>{!! $versionamento->descricao !!}</td>
+                                    <td>{{ \Carbon\Carbon::parse($versionamento->created_at)->format('d/m/Y') }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
-
-    <div class="text-center mt-4">
-        <a href="{{ url()->previous() }}" class="btn btn-secondary"><i class="bi bi-arrow-left"></i> Voltar</a>
-    </div>
 </div>
-
-@endsection
