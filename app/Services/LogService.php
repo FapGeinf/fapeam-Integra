0aa27f62a5238675cbfadcac86c09191e6ddc731
LuizFelipe2023 <luizfelipefroisneves@gmail.com>
2025-05-23 08:42:51 -0400

    ajustes no logservice.

--- app/Services/LogService.php
+++ app/Services/LogService.php
@@ -4,23 +4,16 @@ namespace App\Services;
 use App\Models\Log as LogModel;
 use Log;
 
-class LogService{
+class LogService
+{
 
     public function insertLog(array $data)
     {
-        try {
-            return LogModel::create([
-                'acao' => $data['acao'],
-                'descricao' => $data['descricao'],
-                'user_id' => $data['user_id']
-            ]);
-        } catch (\Exception $e) {
-            Log::error('Erro ao inserir log: ' . $e->getMessage(), [
-                'dados_recebidos' => $data,
-                'trace' => $e->getTraceAsString()
-            ]);
-            return false;
-        }
+        return LogModel::create([
+            'acao' => $data['acao'],
+            'descricao' => $data['descricao'],
+            'user_id' => $data['user_id']
+        ]);
     }
-    
+
 }
