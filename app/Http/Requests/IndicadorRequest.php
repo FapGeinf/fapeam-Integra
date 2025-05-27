845aa266b6d740bb0ed70f0801a0c8e972109d44
LuizFelipe2023 <luizfelipefroisneves@gmail.com>
2025-05-27 13:52:13 -0400

    ajuste no request do objeto indicador.

--- app/Http/Requests/IndicadorRequest.php
+++ app/Http/Requests/IndicadorRequest.php
@@ -24,20 +24,26 @@ class IndicadorRequest extends FormRequest
     public function rules()
     {
         return [
-            'nomeIndicador' => 'nullable|string|max:255',
-            'descricaoIndicador' => 'nullable|string',
-            'eixo_fk' => 'nullable|exists:eixos,id',
+            'nomeIndicador' => 'required|string|max:255',
+            'descricaoIndicador' => 'required|string',
+            'eixo_fk' => 'required|exists:eixos,id',
         ];
     }
 
     public function messages()
     {
         return [
+            'nomeIndicador.required' => 'O campo Nome do Indicador é obrigatório.',
             'nomeIndicador.string' => 'Por favor, o campo Nome do Indicador deve ser do tipo texto.',
             'nomeIndicador.max' => 'O nome não deve exceder o limite de 255 caracteres.',
+
+            'descricaoIndicador.required' => 'O campo Descrição é obrigatório.',
             'descricaoIndicador.string' => 'O campo Descrição deve ser do tipo texto.',
+
+            'eixo_fk.required' => 'Por favor, selecione um eixo.',
             'eixo_fk.exists' => 'Por favor, escolha um eixo que exista.'
         ];
     }
+
 
 }
