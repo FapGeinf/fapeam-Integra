4a247387956aa2821918809b53ece923cb72f292
LuizFelipe2023 <luizfelipefroisneves@gmail.com>
2025-06-16 15:05:43 -0400

    limitando a visualização do link de versionamentos apenas para o usuario root.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -113,12 +113,14 @@
                   </a>
               </li>
             @endif
-            <li class="li-navbar2">
-                <a href="{{ route('versionamentos.index') }}" class="d-flex li-a a-navbar">
-                    <i class="bi bi-files i-navbar mt-1px"></i>
-                    <span class="a-span">Versionamentos</span>
-                </a>
-            </li>
+            @if(Auth::user()->usuario_tipo_fk == 4)
+              <li class="li-navbar2">
+                  <a href="{{ route('versionamentos.index') }}" class="d-flex li-a a-navbar">
+                      <i class="bi bi-files i-navbar mt-1px"></i>
+                      <span class="a-span">Versionamentos</span>
+                  </a>
+              </li>
+            @endif
           @endif
         </ul>
       </div>
