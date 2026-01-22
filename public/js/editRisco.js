f1b55b453c4297fced6392089eab11742ebabede
Leo S. <contato.prof.leonardo@gmail.com>
2026-01-22 11:03:19 -0400

    Padronização da página

--- public/js/editRisco.js
+++ public/js/editRisco.js
@@ -1,80 +1,69 @@
 function showConfirmationModal() {
-    let riscoAno = document.getElementById('riscoAno').value;
-    let responsavelRisco = document.getElementById('responsavelRisco').value;
-    let riscoEvento = CKEDITOR.instances.riscoEvento.getData();
-    let riscoCausa = CKEDITOR.instances.riscoCausa.getData();
-    let riscoConsequencia = CKEDITOR.instances.riscoConsequencia.getData();
-    let nivel_de_risco = document.getElementById('nivel_de_risco').value;
-    let unidadeId = document.querySelector('[name="unidadeId"]').options[document.querySelector(
-        '[name="unidadeId"]').selectedIndex].text;
+  let riscoAno = document.getElementById('riscoAno').value;
+  let responsavelRisco = document.getElementById('responsavelRisco').value;
+  let riscoEvento = CKEDITOR.instances.riscoEvento.getData();
+  let riscoCausa = CKEDITOR.instances.riscoCausa.getData();
+  let riscoConsequencia = CKEDITOR.instances.riscoConsequencia.getData();
+  let nivel_de_risco = document.getElementById('nivel_de_risco').value;
+  let unidadeId = document.querySelector('[name="unidadeId"]').options[document.querySelector(
+    '[name="unidadeId"]').selectedIndex].text;
 
-    let modalContent = `
+  let modalContent = `
+    <div class="row g-3">
+      <div class="col-12 col-sm-4">
+        <div>Ano:</div>
+        <div class="form-control input-disabled">${riscoAno}</div>
+      </div>
 
-        <div class="row g-3 mb-3">
-            <div class="col-sm-4">
-                <div>Ano:</div>
-                <div class="modalBg form-control">${riscoAno}</div>
-            </div>
+      <div class="col-12 col-sm-8">
+        <div>Unidade:</div>
+        <div class="form-control input-disabled">${unidadeId}</div>
+      </div>
 
-            <div class="col-sm-8">
-                <div>Unidade:</div>
-                <div class="modalBg form-control">${unidadeId}</div>
-            </div>
-        </div>
+      <div class="col-12">
+        <div>Responsável:</div>
+        <div class="form-control input-disabled">${responsavelRisco}</div>
+      </div>
 
-        <div class="row g-3 mb-3">
-            <div class="col-sm-12">
-                <div>Responsável:</div>
-                <div class="modalBg form-control">${responsavelRisco}</div>
-            </div>
-        </div>
+      <div class="col-12">
+        <div>Evento de Risco:</div>
+        <div class="form-control input-disabled">${riscoEvento}</div>
+      </div>
 
-        <div class="row g-3 mb-3">
-            <div class="col-sm-12">
-                <div>Evento de Risco:</div>
-                <div class="modalBg form-control">${riscoEvento}</div>
-            </div>
-        </div>
+      <div class="col-12">
+        <div>Causa do Risco:</div>
+        <div class="form-control input-disabled">${riscoCausa}</div>
+      </div>        
 
-        <div class="row g-3 mb-3">
-            <div class="col-sm-12">
-                <div>Causa do Risco:</div>
-                <div class="modalBg form-control">${riscoCausa}</div>
-            </div>
-        </div>
+      <div class="col-12">
+        <div>Causa da consequência:</div>
+        <div class="form-control input-disabled">${riscoConsequencia}</div>
+      </div>
+    </div>
+  `;
 
-        <div class="row g-3 mb-3">
-            <div class="col-sm-12">
-                <div>Causa da consequência:</div>
-                <div class="modalBg form-control">${riscoConsequencia}</div>
-            </div>
-        </div>
+  document.getElementById('modalContent').innerHTML = modalContent;
 
-        <hr>
-        <p class="text-center mb-0">Deseja realmente salvar as alterações?</p>
-    `;
-
-    document.getElementById('modalContent').innerHTML = modalContent;
-
-    let confirmationModal = new bootstrap.Modal(document.getElementById('confirmationModal'));
-    confirmationModal.show();
+  let confirmationModal = new bootstrap.Modal(document.getElementById('confirmationModal'));
+  confirmationModal.show();
 }
 
 function submitForm() {
-    document.getElementById('formCreate').submit();
+  document.getElementById('formCreate').submit();
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    var ckeditorConfig = {
-        extraPlugins: 'wordcount',
-        wordcount: {
-            showCharCount: true,
-            maxCharCount: 10000,
-            maxCharCountMsg: 'Você atingiu o limite máximo de caracteres permitidos.',
-            charCountMsg: 'Caracteres restantes: {0}'
-        }
-    };
-    CKEDITOR.replace('riscoEvento',ckeditorConfig);
-    CKEDITOR.replace('riscoCausa',ckeditorConfig);
-    CKEDITOR.replace('riscoConsequencia',ckeditorConfig);
+  var ckeditorConfig = {
+    extraPlugins: 'wordcount',
+    wordcount: {
+      showCharCount: true,
+      maxCharCount: 10000,
+      maxCharCountMsg: 'Você atingiu o limite máximo de caracteres permitidos.',
+      charCountMsg: 'Caracteres restantes: {0}'
+    }
+  };
+
+  CKEDITOR.replace('riscoEvento',ckeditorConfig);
+  CKEDITOR.replace('riscoCausa',ckeditorConfig);
+  CKEDITOR.replace('riscoConsequencia',ckeditorConfig);
 });
