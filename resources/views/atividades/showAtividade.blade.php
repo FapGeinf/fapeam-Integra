34b6a392749bafaf72ff479e9d85d3634ec1afed
LuizFelipe2023 <luizfelipefroisneves@gmail.com>
2026-04-06 16:35:51 -0400

    reestruturando a show de atividade, estava esquisita visualmente, provavelmente precisará de um refino a mais, mas esta versão esta ligeramente melhor que a anterior.

--- resources/views/atividades/showAtividade.blade.php
+++ resources/views/atividades/showAtividade.blade.php
@@ -3,138 +3,114 @@
 @section('content')
 
 <link rel="stylesheet" href="{{ asset('css/edit.css') }}">
-<link rel="stylesheet" href="{{ asset('css/show.css') }}">
+<link rel="stylesheet" href="{{ asset('css/main.css') }}">
 <link rel="stylesheet" href="{{ asset('css/buttons.css') }}">
+<style>
+    .form-control:disabled, .form-select:disabled {
+        background-color: #f8f9fa;
+        cursor: not-allowed;
+        opacity: 1; /* Mantém a cor do texto nítida */
+    }
+    .ck-editor__main { cursor: not-allowed; }
+</style>
 
 <div class="container pt-5" style="max-width: 800px;">
-	<div class="col-12 border box-shadow">
-		<h5 class="text-center">Detalhes da Atividade</h5>
+    <div class="col-12 border box-shadow p-4 bg-white">
+        <h5 class="text-center mb-4">Detalhes da Atividade</h5>
 
-		<h6 class="col-12 text-center fw-semibold text-secondary">
-			{!! $atividade->atividade_descricao !!}
-		</h6>
+        <div class="row g-3">
+            <div class="col-12">
+                <label class="fw-bold">Eixos:</label>
+                <div class="form-control" style="background: #f8f9fa; min-height: 45px;">
+                    @forelse($atividade->eixos as $eixo)
+                        <span class="badge bg-secondary me-1">Eixo {{ $eixo->id }} - {{ $eixo->nome }}</span>
+                    @empty
+                        <span class="text-muted">Nenhum eixo associado</span>
+                    @endforelse
+                </div>
+            </div>
 
-		<div class="row g-3">
-			<div class="col-12">
-				<label class="">Eixos:</label>
+            <div class="col-12 mt-2">
+                <label class="fw-bold">Responsável:</label>
+                <input class="form-control" value="{{ $atividade->responsavel ?? 'Não informado' }}" disabled>
+            </div>
 
-				<div class="form-control input-disabled">
-					@if($atividade->eixos->count() > 0)
-					<ul>
-						@foreach($atividade->eixos as $eixo)
-							<li>EIXO {{$eixo->id}} - {{ $eixo->nome }}</li>
-						@endforeach
-					</ul>
+            <div class="col-12 mt-4">
+                <label class="fw-bold">Atividade:</label>
+                <div class="form-control" style="background: #f8f9fa; height: auto; min-height: 100px;">
+                    {!! $atividade->atividade_descricao !!}
+                </div>
+            </div>
 
-					@else
-						Nenhum eixo associado
-					@endif
-				</div>
-			</div>
+            <div class="col-12">
+                <label class="fw-bold">Objetivo:</label>
+                <div class="form-control" style="background: #f8f9fa; height: auto; min-height: 100px;">
+                    {!! $atividade->objetivo !!}
+                </div>
+            </div>
 
-			<div class="col-12">
-				<label class="">Responsável:</label>
+            <div class="col-12 col-md-6">
+                <label class="fw-bold">Público Alvo:</label>
+                <input class="form-control" value="{{ $atividade->publico->nome ?? 'Não informado' }}" disabled>
+            </div>
 
-				<div class="form-control input-disabled">
-					@if(!empty($atividade->responsavel))
-						{{ $atividade->responsavel }}
-					@else
-						<span>Não inserido</span>
-					@endif
-				</div>
-			</div>
+            <div class="col-12 col-md-6">
+                <label class="fw-bold">Tipo de Evento:</label>
+                @php
+                    $tipos = [0 => 'Sem evento', 1 => 'Presencial', 2 => 'Online', 3 => 'Presencial e Online'];
+                @endphp
+                <input class="form-control" value="{{ $tipos[$atividade->tipo_evento] ?? 'Não informado' }}" disabled>
+            </div>
 
-			<div class="col-12">
-				<label class="">Objetivo:</label>
+            <div class="col-12 mt-1">
+                <label class="fw-bold">Canal de Divulgação:</label>
+                <div class="form-control" style="background: #f8f9fa; min-height: 45px;">
+                    @forelse($atividade->canais as $canal)
+                        <span class="badge bg-info text-dark me-1">{{ $canal->nome }}</span>
+                    @empty
+                        <span class="text-muted">Nenhum canal associado</span>
+                    @endforelse
+                </div>
+            </div>
 
-				<div class="form-control input-disabled pb-0">
-					{!! $atividade->objetivo !!}
-				</div>
-			</div>
+            <div class="col-12 mt-1">
+                <label class="fw-bold">Indicadores:</label>
+                <div class="form-control" style="background: #f8f9fa; min-height: 45px;">
+                    @forelse($atividade->indicadores as $indicador)
+                        <span class="badge bg-primary me-1">{{ $indicador->nomeIndicador }}</span>
+                    @empty
+                        <span class="text-muted">Nenhum indicador associado</span>
+                    @endforelse
+                </div>
+            </div>
 
-			<div class="col-12 col-sm-6">
-				<label class="">Público Alvo:</label>
+            <div class="col-12 col-md-6">
+                <label class="fw-bold">Data Realizada:</label>
+                <input class="form-control" value="{{ $atividade->data_realizada ? \Carbon\Carbon::parse($atividade->data_realizada)->format('d/m/Y') : 'N/A' }}" disabled>
+            </div>
 
-				<div class="form-control input-disabled">
-					{{ $atividade->publico ? $atividade->publico->nome : 'Não informado' }}
-				</div>
-			</div>
+            <div class="col-12 col-md-6">
+                <label class="fw-bold">Data Prevista:</label>
+                <input class="form-control" value="{{ $atividade->data_prevista ? \Carbon\Carbon::parse($atividade->data_prevista)->format('d/m/Y') : 'N/A' }}" disabled>
+            </div>
 
-			<div class="col-12 col-sm-6">
-				<label class="">Tipo de evento:</label>
+            <div class="col-12 col-md-6">
+                <label class="fw-bold">Previsto:</label>
+                <div class="input-group">
+                    <input class="form-control" value="{{ $atividade->meta }}" disabled>
+                    <span class="input-group-text">{{ $atividade->medida->nome ?? '' }}</span>
+                </div>
+            </div>
 
-				<div class="form-control input-disabled">
-					@if($atividade->tipo_evento == 1)
-						Presencial
-
-					@elseif($atividade->tipo_evento == 2)
-						Online
-
-					@elseif($atividade->tipo_evento == 3)
-						Presencial e Online
-
-					@else
-						Sem evento
-					@endif
-				</div>
-			</div>
-
-			<div class="col-12">
-				<label class="">Canais:</label>
-
-				<div class="form-control input-disabled">
-					@if($atividade->canais->count() > 0)
-						<ul>
-							@foreach($atividade->canais as $canal)
-								<li>{{ $canal->nome }}</li>
-							@endforeach
-						</ul>
-					@else
-						Nenhum canal associado
-					@endif
-				</div>
-			</div>
-
-			<div class="col-12">
-				<label class="">Indicadores:</label>
-
-				<div class="form-control input-disabled">
-					@if($atividade->indicadores->count() > 0)
-						<ul>
-							@foreach($atividade->indicadores as $indicador)
-								<li>{{ $indicador->nomeIndicador }}</li>
-							@endforeach
-						</ul>
-					@else
-						Nenhum indicador associado
-					@endif
-				</div>
-			</div>
-
-			<div class="col-12 col-sm-6">
-				<label class="">Previsto:</label>
-
-				<div class="form-control input-disabled">
-					{{ $atividade->meta }} {{ $atividade->medida->nome ?? 'N/A' }}
-				</div>
-			</div>
-
-			<div class="col-12 col-sm-6">
-				<label class="">Realizado:</label>
-				<div class="form-control input-disabled">
-					{{ $atividade->realizado }} {{ $atividade->medida->nome ?? 'N/A' }}
-				</div>
-			</div>
-
-			<div class="col-12 col-sm-6">
-				<label class="">Data Término:</label>
-				<div class="form-control input-disabled">
-					{{ \Carbon\Carbon::parse($atividade->data_realizada)->translatedFormat('d \d\e F \d\e Y') }}
-				</div>
-			</div>
-
-		</div>
-	</div>
+            <div class="col-12 col-md-6">
+                <label class="fw-bold">Realizado:</label>
+                <div class="input-group">
+                    <input class="form-control" value="{{ $atividade->realizado }}" disabled>
+                    <span class="input-group-text">{{ $atividade->medida->nome ?? '' }}</span>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <x-back-button/>
