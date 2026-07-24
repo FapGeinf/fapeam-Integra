3f33eae0fc5512458f3b9408b84d33b2d514bcfb
Leo S. <contato.prof.leonardo@gmail.com>
2026-07-24 09:48:48 -0400

    Update dark mode styling and hide navbar

--- resources/views/links_login/apresentacao.blade.php
+++ resources/views/links_login/apresentacao.blade.php
@@ -5,7 +5,7 @@
 <link rel="stylesheet" href="{{asset('css/main.css')}}">
 <link rel="stylesheet" href="{{asset('css/buttons.css')}}">
 
-<nav class="navbar navbar-expand-lg border-bottom sticky-top">
+{{-- <nav class="navbar navbar-expand-lg border-bottom sticky-top">
   <div class="container">
     <span class="navbar-brand text-white mb-0">Legislação</span>
 
@@ -23,26 +23,26 @@
       </a>
     </div>
   </div>
-</nav>
+</nav> --}}
 
 <main class="container my-4 pt-5">
   <div class="card box-shadow">
     <div class="card-body p-2">
       <h1 class="h5 mb-3">Bem-vindo(a) ao Sistema Íntegra!</h1>
 
-      <p class="mb-3">
+      <p class="mb-3 intro">
         O Sistema Íntegra é uma ferramenta do Programa de Integridade da Fundação de Amparo à Pesquisa do Estado do Amazonas – FAPEAM.
       </p>
 
-      <p class="mb-3">
+      <p class="mb-3 intro">
         Sua finalidade é aprimorar as rotinas e sistemas de controle interno preventivo e corretivo, buscando assegurar a legalidade, legitimidade, economicidade, eficiência, publicidade e transparência da gestão administrativa, proporcionando apoio à Alta Administração na gestão dos recursos públicos e ao atendimento às legislações vigentes.
       </p>
 
-      <p class="mb-3">
+      <p class="mb-3 intro">
         Esta ferramenta acompanha as rotinas administrativas, com intuito de direcionar, monitorar e avaliar a efetividade do Programa de Integridade, conforme os eixos estabelecidos:
       </p>
 
-      <ul class="mb-3 ps-3">
+      <ul class="mb-3 ps-3 intro">
         <li class="mb-1">I – Comprometimento e Apoio da Alta Direção;</li>
         <li class="mb-1">II – Institucionalização do Código de Conduta;</li>
         <li class="mb-1">III – Avaliação de Riscos;</li>
@@ -53,7 +53,7 @@
         <li>VIII – Monitoramento Contínuo.</li>
       </ul>
 
-      <p>
+      <p class="intro">
         Contamos com sua contribuição para prevenir, detectar e remediar fraudes e atos de corrupção em apoio à boa governança.
       </p>
     </div>
