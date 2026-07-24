448a8c4d6500cb7a3d7cc3a23907dfff7c0f1310
Leo S. <contato.prof.leonardo@gmail.com>
2026-07-24 15:58:16 -0400

    Update legislacao.blade.php

--- resources/views/links_login/legislacao.blade.php
+++ resources/views/links_login/legislacao.blade.php
@@ -5,7 +5,7 @@
 <link rel="stylesheet" href="{{asset('css/main.css')}}">
 <link rel="stylesheet" href="{{asset('css/buttons.css')}}">
 
-<nav class="navbar navbar-expand-lg border-bottom sticky-top">
+{{-- <nav class="navbar navbar-expand-lg border-bottom sticky-top">
   <div class="container">
     <span class="navbar-brand text-white mb-0">Legislação</span>
 
@@ -23,7 +23,7 @@
       </a>
     </div>
   </div>
-</nav>
+</nav> --}}
 
 <main class="container my-4 pt-5">
   <div class="card box-shadow">
@@ -31,37 +31,37 @@
       <h1 class="h5 mb-3">Normativos e documentos</h1>
 
       <ol class="mb-0 ps-3">
-        <li class="mb-3">
+        <li class="mb-3 intro">
           Portaria da Controladoria Geral da União - CGU N.° 1.089, DE 25 DE ABRIL DE 2018. Estabelece orientações
           para os órgãos e as entidades da administração pública federal direta, autarquias e fundacional, adotem
           procedimentos para estruturação, a execução e o monitoramento de seus programas de integridade e dá outras
           providências.
         </li>
 
-        <li class="mb-3">
+        <li class="mb-3 intro">
           INSTRUÇÃO NORMATIVA Nº 02, DE 28 DE NOVEMBRO DE 2022. Dispõe sobre as diretrizes a serem observadas na
           implementação do Programa de Integridade, no âmbito dos órgãos e das entidades da Administração Pública
           Estadual Direta e Indireta, e dá outras providências.
         </li>
 
-        <li class="mb-3">
+        <li class="mb-3 intro">
           INSTRUÇÃO NORMATIVA Nº 03, DE 28 DE NOVEMBRO DE 2022. Disciplina os procedimentos para a implantação do
           Programa de Integridade de fornecedores, no âmbito do Poder Executivo do Estado do Amazonas e dá outras
           providências.
         </li>
 
-        <li class="mb-3">
+        <li class="mb-3 intro">
           DECRETO N.° 4.849, DE 14 DE OUTUBRO DE 2019. Disciplina a Política de Governança e Gestão do Estado do
           Amazonas e dá outras providências.
         </li>
 
-        <li class="mb-3">
+        <li class="mb-3 intro">
           DECRETO N.° 42.873, DE 14 DE OUTUBRO DE 2020. INSTITUI a Unidade de Controle Interno - UCI, no âmbito da
           Fundação de Amparo à Pesquisa do Estado do Amazonas - FAPEAM, estabelece diretrizes para sua estruturação
           e funcionamento e dá outras providências.
         </li>
 
-        <li class="mb-3">
+        <li class="mb-3 intro">
           Manual de Condutas Éticas e de Integridade da FAPEAM
           <a class="link-primary text-break"
               href="https://www.fapeam.am.gov.br/wpcontent/uploads/2024/01/manual_de_conduta_atualizado_08012024.pdf"
@@ -70,7 +70,7 @@
           </a>
         </li>
 
-        <li class="mb-3">
+        <li class="mb-3 intro">
           Manual Prático de Sindicância Disciplinar da FAPEAM
           <a class="link-primary text-break"
               href="https://www.fapeam.am.gov.br/wp-content/uploads/2024/01/Manual-Pratico-de-Sindicancia-Disciplinar-da-FAPEAM.pdf"
@@ -79,15 +79,15 @@
           </a>
         </li>
 
-        <li class="mb-3">
+        <li class="mb-3 intro">
           Declaração de Posicionamento do Instituto Internacional de Auditores - IIA: As três linhas de defesa. Ano 2013.
         </li>
 
-        <li class="mb-3">
+        <li class="mb-3 intro">
           Declaração de Posicionamento do Instituto Internacional de Auditores - IIA: As três linhas de defesa: Uma atualização das três linhas. Ano 2020.
         </li>
 
-        <li class="mb-0">
+        <li class="mb-0 intro">
           Decreto n° 50.868, de 12 de dezembro de 2024, que institui o Programa Estadual de Integridade no âmbito da
           Administração Pública Direta e Indireta do Poder Executivo do Estado do Amazonas, e dá outras providências.
         </li>
