18c82628598598c5a0272e51673c52e6c07e41ca
LuizFelipe2023 <luizfelipefroisneves@gmail.com>
2026-09-18 11:02:59 -0400

    mais um ajuste de formatação na tela de plano de acao.

--- resources/views/atividades/plano_acao.blade.php
+++ resources/views/atividades/plano_acao.blade.php
@@ -82,14 +82,14 @@
           </div>
 
           <!-- <div class="col-12 col-sm-6 col-md-2">
-              <label for="filter-data" class="f-size">Ordenar por data prevista:</label>
-              <select name="filter-data" id="filter-data" class="form-select input-enabled f-size border-grey">
-                <option selected disabled>Escolha uma opção</option>
-                <option value="">Todos</option>
-                <option value="asc">Mais Antiga</option>
-                <option value="desc">Mais Recente</option>
-              </select>
-            </div> -->
+                <label for="filter-data" class="f-size">Ordenar por data prevista:</label>
+                <select name="filter-data" id="filter-data" class="form-select input-enabled f-size border-grey">
+                  <option selected disabled>Escolha uma opção</option>
+                  <option value="">Todos</option>
+                  <option value="asc">Mais Antiga</option>
+                  <option value="desc">Mais Recente</option>
+                </select>
+              </div> -->
 
           <div class="col-12 col-sm-6 col-md-2">
             <label for="filter-ano" class="f-size">Filtrar por ano:</label>
@@ -253,31 +253,31 @@
                   </td>
 
                   <!-- <td class="text-center">
-                          @switch($atividade->statusAtividade->nome ?? '')
-                            @case('Acompanhamento')
-                              <div class="badge bg-warning text-dark">
-                                {{ $atividade->statusAtividade->nome }} <i class="bi bi-clock-history ms-1"></i>
-                              </div>
-                            @break
+                              @switch($atividade->statusAtividade->nome ?? '')
+                                @case('Acompanhamento')
+                                  <div class="badge bg-warning text-dark">
+                                    {{ $atividade->statusAtividade->nome }} <i class="bi bi-clock-history ms-1"></i>
+                                  </div>
+                                @break
 
-                            @case('Executado')
-                              <div class="badge bg-success">
-                                {{ $atividade->statusAtividade->nome }} <i class="bi bi-check-circle ms-1"></i>
-                              </div>
-                            @break
+                                @case('Executado')
+                                  <div class="badge bg-success">
+                                    {{ $atividade->statusAtividade->nome }} <i class="bi bi-check-circle ms-1"></i>
+                                  </div>
+                                @break
 
-                            @case('Não Executado')
-                              <div class="badge bg-danger">
-                                {{ $atividade->statusAtividade->nome }} <i class="bi bi-x-circle ms-1"></i>
-                              </div>
-                            @break
+                                @case('Não Executado')
+                                  <div class="badge bg-danger">
+                                    {{ $atividade->statusAtividade->nome }} <i class="bi bi-x-circle ms-1"></i>
+                                  </div>
+                                @break
 
-                          @default
-                            <div class="badge bg-secondary">
-                              {{ $atividade->statusAtividade->nome ?? 'N/A' }} <i class="bi bi-question-circle ms-1"></i>
-                            </div>
-                          @endswitch
-                        </td> -->
+                              @default
+                                <div class="badge bg-secondary">
+                                  {{ $atividade->statusAtividade->nome ?? 'N/A' }} <i class="bi bi-question-circle ms-1"></i>
+                                </div>
+                              @endswitch
+                            </td> -->
 
                   <td class="text-center">
                     @if(Auth::user()->unidade->unidadeTipoFK == 1 || Auth::user()->usuario_tipo_fk == 1 || Auth::user()->usuario_tipo_fk == 4)
